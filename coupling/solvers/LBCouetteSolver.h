d5267bc7b5e21d53474292d9bf024df48a004883
Piet Jarmatz <piet.jarmatz@tum.de>
2018-06-20 21:36:39 +0200

    USHER bugfix update signAl

--- coupling/solvers/LBCouetteSolver.h
+++ coupling/solvers/LBCouetteSolver.h
@@ -244,7 +244,7 @@ class coupling::solvers::LBCouetteSolver: public coupling::solvers::AbstractCoue
         globalCellCoords[0] = (globalCellCoords[0]+_offset[0]) - coords[0]*_avgDomainSizeX;
         globalCellCoords[1] = (globalCellCoords[1]+_offset[1]) - coords[1]*_avgDomainSizeY;
         globalCellCoords[2] = (globalCellCoords[2]+_offset[2]) - coords[2]*_avgDomainSizeZ;
-std::cout << "Process coords: " << coords << ":  GlobalCellCoords for index " << indexConversion.getGlobalVectorCellIndex(recvIndices[i]) << ": " << globalCellCoords << std::endl;
+//std::cout << "Process coords: " << coords << ":  GlobalCellCoords for index " << indexConversion.getGlobalVectorCellIndex(recvIndices[i]) << ": " << globalCellCoords << std::endl;
         const int index = get(globalCellCoords[0],globalCellCoords[1],globalCellCoords[2]);
         #if (COUPLING_MD_DEBUG==COUPLING_MD_YES)
         if (_flag[index]!=MD_BOUNDARY){std::cout << "ERROR LBCouetteSolver::setMDBoundaryValues(): Cell " << index << " is no MD boundary cell!" << std::endl; exit(EXIT_FAILURE);}
