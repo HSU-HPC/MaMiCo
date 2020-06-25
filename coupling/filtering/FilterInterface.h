cbe6ec13ced1b26d57aead52544f229e45ca936e
Felix Maurer <felix.maurer@informatik.uni-hamburg.de>
2020-06-25 14:10:04 +0200

    Fix FilterInterface.h

--- coupling/filtering/FilterInterface.h
+++ coupling/filtering/FilterInterface.h
@@ -25,16 +25,16 @@ class coupling::FilterInterface{
 
 		//Converts indices, initializes domain_* member variables. If a filter always uses the entire MD domain, you do not need to implement this.
 		//Called from coupling::FilterSequence<dim>::fillSequenceData(...)
-		virtual void initDomain();
+		//virtual void initDomain();
 
 		//Wecause we don't want to call initDomain() each time coupling::FilterSequence<dim>::fillSequenceData(...) is called,
 		//we need an option to check if it's been loaded before.
-		void isDomainLoaded() { return _domainLoaded;}
+		bool isDomainLoaded() { return _domainLoaded;}
 	private:
 		//filter domain is determined by spanning a <dim>-dimensional space between two vectors.
 		//initiliazed within the filter's constructor, uses XML attributes of the corresponding filter node as input
-		tarch::la::Vector<dim> _domainStart;
-		tarch::la::Vector<dim> _domainEnd;
+		tarch::la::Vector<dim, unsigned int> _domainStart;
+		tarch::la::Vector<dim, unsigned int> _domainEnd;
 
 		
 		std::vector<coupling::datastructures::MacroscopicCell<dim>* > _domainCells;
