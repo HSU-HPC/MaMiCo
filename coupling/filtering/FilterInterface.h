bc1e26cd138f4ed62bbfaf9609d2b6e86e970b99
Felix Maurer <felix.maurer@informatik.uni-hamburg.de>
2020-07-19 23:10:57 +0200

    Reworked vector swapping and domain definitions

--- coupling/filtering/FilterInterface.h
+++ coupling/filtering/FilterInterface.h
@@ -16,33 +16,16 @@ template<unsigned int dim>
 class coupling::FilterInterface{
 	public:
 
-		//Use this in case your filter uses the entire input domain
-		//TODO: Problem: The hosting FilterSequence() does not know its cell vector (inputCellVector) or its size during initialization
 		FilterInterface(
 				const std::vector<coupling::datastructures::MacroscopicCell<dim> *>& inputCellVector,
 				const std::vector<coupling::datastructures::MacroscopicCell<dim> *>& outputCellVector,
 				const std::vector<tarch::la::Vector<dim, unsigned int>> cellIndices): //covers the entire MD domain
 				
-				_domainInputCells(inputCellVector),
-				_domainOutputCells(outputCellVector),
-				_domainCellIndices(cellIndices),
-				_domainStart(cellIndices[0]),
-				_domainEnd(cellIndices.back())
+				_inputCells(inputCellVector),
+				_outputCells(outputCellVector),
+				_cellIndices(cellIndices)
 		{}
 
-		//Use this in case your filter uses a subdomain of the input domain
-		FilterInterface(
-				const std::vector<coupling::datastructures::MacroscopicCell<dim> *>& inputCellVector,
-				const std::vector<coupling::datastructures::MacroscopicCell<dim> *>& outputCellVector,
-				const std::vector<tarch::la::Vector<dim, unsigned int>> cellIndices, //covers the entire MD domain
-				tarch::la::Vector<dim, unsigned int> domainStart, //unit: number of macroscopic cells
-				tarch::la::Vector<dim, unsigned int> domainEnd):
-				
-				_domainStart(domainStart),
-				_domainEnd(domainEnd)
-		{
-			initDomain(inputCellVector, outputCellVector, cellIndices);
-		}
 
 		virtual ~FilterInterface(){};
 
@@ -50,53 +33,7 @@ class coupling::FilterInterface{
 		//Applies the filter to all cells that are within the filter's domain.
 		virtual void operator()() = 0;
 	protected:
-		std::vector<coupling::datastructures::MacroscopicCell<dim>* > _domainInputCells; //pointers to (parts of) either seqCellVec1 or seqCellVec2	
-		std::vector<coupling::datastructures::MacroscopicCell<dim>* > _domainOutputCells; //pointers to (parts of) of the other seqCellVec
-		std::vector<tarch::la::Vector<dim,unsigned int>> _domainCellIndices; //indices for both domainCells vectors is the same
-
-
-		//filter domain is determined by spanning a <dim>-dimensional space between two vectors.
-		//initiliazed within the filter's constructor, uses XML attributes of the corresponding filter node as input
-		tarch::la::Vector<dim, unsigned int> _domainStart;
-		tarch::la::Vector<dim, unsigned int> _domainEnd;
-
-	private:
-		void initDomain(
-					const std::vector<coupling::datastructures::MacroscopicCell<dim> *>& inputCellVector,
-					const std::vector<coupling::datastructures::MacroscopicCell<dim> *>& outputCellVector,
-					const std::vector<tarch::la::Vector<dim, unsigned int>> cellIndices
-		){
-			#ifdef DEBUG_FILTER
-			std::cout << "FilterInterface: Initializing filter. Input vector has " << inputCellVector.size() << " cells. ";
-			#endif
-			//sanity check
-			if(inputCellVector.size() != cellIndices.size() || outputCellVector.size() != cellIndices.size()){
-				std::cout << "Cell and index vector out of synch. Aborting.";
-				exit(EXIT_FAILURE);
-			}
-		
-			//you could just as well use outputCellVector instead of inputCellIndices to iterate	
-			for(unsigned int d = 0; d < dim; d++) if(cellIndices.back()[d] < _domainEnd[d]){
-				std::cout << "Filter domain size larger than MD domain. Aborting." << std::endl;
-				exit(EXIT_FAILURE);
-			}
-			bool outOfBounds;
-			for(unsigned int index = 0; index < inputCellVector.size(); index++){
-				outOfBounds = false;
-				for(unsigned int d = 0; d < dim; d++) 
-					//"index" does not reference the dim-dimensional indices stored in e.g. cellIndices but the cell vectors' linear index.
-					if(cellIndices[index][d] < _domainStart[d] || cellIndices[index][d] > _domainEnd[d]) outOfBounds = true;
-				if(!outOfBounds){
-					_domainInputCells.push_back(inputCellVector[index]);
-					_domainOutputCells.push_back(outputCellVector[index]);
-					_domainCellIndices.push_back(cellIndices[index]);
-				}
-			}
-
-			#ifdef DEBUG_FILTER
-			std::cout << "Domain vector has " << _domainInputCells.size() << " cells." << std::endl;
-			#endif
-
-		}
-
+		std::vector<coupling::datastructures::MacroscopicCell<dim>* > _inputCells; 	
+		std::vector<coupling::datastructures::MacroscopicCell<dim>* > _outputCells;
+		std::vector<tarch::la::Vector<dim,unsigned int>> _cellIndices; //indices for both cell vectors are identical
 };
--- coupling/filtering/FilterSequence.h
+++ coupling/filtering/FilterSequence.h
@@ -22,10 +22,31 @@ namespace coupling{
 template<unsigned int dim>
 class coupling::FilterSequence {
 	public:
-    	FilterSequence(const coupling::IndexConversion<dim>* indexConversion, const char* name, std::vector<tarch::la::Vector<dim, unsigned int>> cellIndices) :
-    	_indexConversion(indexConversion), _name(name), _cellIndices(cellIndices), _isOutput(false){
-        	#ifdef DEBUG_FILTER_PIPELINE
-        	std::cout << "FP: Created new sequence named " << _name << "." << std::endl;
+    	FilterSequence( const coupling::IndexConversion<dim>* indexConversion,
+						const char* name,
+						const std::vector<coupling::datastructures::MacroscopicCell<dim>* >	inputCellVector,
+						std::vector<tarch::la::Vector<dim, unsigned int>> cellIndices,
+						tarch::la::Vector<dim, unsigned int> domainStart,
+						tarch::la::Vector<dim, unsigned int> domainEnd):
+    	_indexConversion(indexConversion), 
+		_name(name), 
+		_inputCellVector(inputCellVector),
+		_cellIndices(cellIndices), 
+		_domainStart(domainStart),
+		_domainEnd(domainEnd),
+		_isOutput(false)
+		{	
+			#ifdef DEBUG_FILTER_PIPELINE
+        	std::cout << "FS (" << _name << "): Now initializing." << std::endl;
+        	#endif
+            for(unsigned int d = 0; d < dim; d++) if(_cellIndices.back()[d] < _domainEnd[d]){
+                std::cout << "Filter domain size larger than MD domain. Aborting." << std::endl;
+                exit(EXIT_FAILURE);
+            }
+			initCellVectors();
+			initDomain();
+			#ifdef DEBUG_FILTER_PIPELINE
+        	std::cout << "FS ("<< _name <<"): Finished initialization " << std::endl;
         	#endif
     	}
 
@@ -35,19 +56,14 @@ class coupling::FilterSequence {
 			for (auto v2 : _cellVector2) delete v2;
 			for (auto f : _filters) delete f;
         	#ifdef DEBUG_FILTER_PIPELINE
-        	std::cout << "FP: Sequence named " << _name << " deconstructed." << std::endl;
+        	std::cout << "FS("<< _name <<"): Deconstructed. " << std::endl;
         	#endif
     	}
 
-
-		//TODO: move to constructor (?)
 		int loadFiltersFromXML(tinyxml2::XMLElement* sequenceNode);
-
-    	void fillSequenceData(const std::vector<coupling::datastructures::MacroscopicCell<dim>* >& inputCellVector);
-		
-		//TODO: besprechen
+					
 		void updateCellVectors(){
-			for(unsigned int index = 0; index < _inputCellVector.size(); index++){
+			for(unsigned int index = 0; index < _cellIndices.size(); index++){
 				*(_cellVector1[index]) = *(_inputCellVector[index]);
 				*(_cellVector2[index]) = *(_inputCellVector[index]);
 			}
@@ -58,26 +74,54 @@ class coupling::FilterSequence {
 		bool isOutput() { return _isOutput; };
 		void setAsOutput() { _isOutput = true; };
 
-    	std::vector<coupling::datastructures::MacroscopicCell<dim>* >& getMacroscopicCells() { return _outputCellVector; } //non const
-    	const std::vector<coupling::datastructures::MacroscopicCell<dim>* >& getMacroscopicCells() const { return _outputCellVector; } //const
+    	const std::vector<coupling::datastructures::MacroscopicCell<dim>* >& getOutputCellVector() const{ 
+			if(_filters.empty()) std::cout << "FS (" << _name << "): Warning: Accessing cell vectors while _filters is empty." << std::endl;
+			if(_filters.size() % 2 == 0) return _cellVector1;
+			else return _cellVector2;
+		}
 
-    	void setCellIndices(const std::vector<tarch::la::Vector<dim, unsigned int>>& cellIndices) { _cellIndices = cellIndices; }
-    	std::vector<tarch::la::Vector<dim, unsigned int>> getCellIndices() { return _cellIndices; }
+    	//void setCellIndices(const std::vector<tarch::la::Vector<dim, unsigned int>>& cellIndices) { _cellIndices = cellIndices; }
+    	//std::vector<tarch::la::Vector<dim, unsigned int>> getCellIndices() { return _cellIndices; }
        
 		std::vector<coupling::FilterInterface<dim> *> getFilters() { return _filters; }	
-        
+
+ 		//TODO: remove
+		void DEBUG_GET_VECTOR_SIZES(){
+			std::cout << "FS (" << _name << "): Printing vector sizes, capacities" << std::endl;
+			std::cout << "_cellVector1: " << _cellVector1.size() << " , " << _cellVector1.capacity() << std::endl;
+			std::cout << "_cellVector2: " << _cellVector2.size() << " , " << _cellVector2.capacity() << std::endl;
+			std::cout << "_cellIndices: " << _cellIndices.size() << " , " << _cellIndices.capacity() << std::endl;
+			std::cout << "_inputDomainCellVector: " << _inputDomainCellVector.size() << " , " << _inputDomainCellVector.capacity() << std::endl;
+			std::cout << "_domainCellVector1: " << _domainCellVector1.size() << " , " << _domainCellVector1.capacity() << std::endl;
+			std::cout << "_domainCellVector2: " << _domainCellVector2.size() << " , " << _domainCellVector1.capacity() << std::endl;
+		}       
 	private:
+		//TODO: move to constructor (?)
+		void initDomain();
+		//TODO: move to constructor (?)
+		void initCellVectors();
+
 		const coupling::IndexConversion<dim>* _indexConversion;
     	const char* _name;
 
-		std::vector<coupling::datastructures::MacroscopicCell<dim>* > _inputCellVector;//pointers to macro cells of this sequence's input
+    	std::vector<coupling::datastructures::MacroscopicCell<dim>* > _inputCellVector;//points to (foreign) input vector
     	std::vector<coupling::datastructures::MacroscopicCell<dim>* > _cellVector1;//allocated for this sequence only
 		std::vector<coupling::datastructures::MacroscopicCell<dim>* > _cellVector2;//allocated for this sequence only
-		std::vector<coupling::datastructures::MacroscopicCell<dim>* > _outputCellVector;//pointers to either _cellVector1 or _cellVector2. Use this as output if _isOutput == true and as input for other sequences
     	std::vector<tarch::la::Vector<dim, unsigned int>> _cellIndices;//all of the above use the same indexing
 
+        /*pseudo const*/std::vector<coupling::datastructures::MacroscopicCell<dim>* > _inputDomainCellVector;//pointers to macro cells of this sequence's input that are within domain
+    	std::vector<coupling::datastructures::MacroscopicCell<dim>* > _domainCellVector1;//points to cells of vector 1 that are within the domain's range
+    	std::vector<coupling::datastructures::MacroscopicCell<dim>* > _domainCellVector2;//same for vector 2
+		tarch::la::Vector<dim, unsigned int> _domainStart;
+		tarch::la::Vector<dim, unsigned int> _domainEnd;
+    	std::vector<tarch::la::Vector<dim, unsigned int>> _globalDomainCellIndices; //uses _cellIndices indexing (i. e. _domainStart .... _domainEnd))
+    	std::vector<tarch::la::Vector<dim, unsigned int>> _localDomainCellIndices; //starts at (0,0,0)
+		
 		bool _isOutput;
 		std::vector<coupling::FilterInterface<dim> *> _filters;
+		
+
+
 };
 
 //inlcude implementation
