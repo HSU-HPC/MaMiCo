8c9d64b5b0a5ba63ef1e5284c9dc8ecbc570f34e
Felix Maurer <felix.maurer@informatik.uni-hamburg.de>
2020-07-24 16:19:47 +0200

    Added comments explaining FilterPipeline and FilterSequence

--- coupling/filtering/FilterInterface.h
+++ coupling/filtering/FilterInterface.h
@@ -11,15 +11,23 @@ namespace coupling{
     class FilterInterface;
 }
 
-
+/**
+ *  Generic interface for filters that are to be applied to data of coupling::MacroscopicCells before MD to Macro transfer.
+ *  Examples for such filters can be found in coupling/filtering/filters.
+ *  @Author Felix Maurer
+ */
 template<unsigned int dim>
 class coupling::FilterInterface{
 	public:
 
+		/*
+		 * Filter constructors are called during instanciation of their corresponding FilterSequence.
+		 * You can customize parameterization in coupling::FilterSequence::loadFiltersFromXML(...).
+		 */
 		FilterInterface(
 				const std::vector<coupling::datastructures::MacroscopicCell<dim> *>& inputCellVector,
 				const std::vector<coupling::datastructures::MacroscopicCell<dim> *>& outputCellVector,
-				const std::vector<tarch::la::Vector<dim, unsigned int>> cellIndices): //covers the entire MD domain
+				const std::vector<tarch::la::Vector<dim, unsigned int>> cellIndices):
 				
 				_inputCells(inputCellVector),
 				_outputCells(outputCellVector),
@@ -30,10 +38,16 @@ class coupling::FilterInterface{
 		virtual ~FilterInterface(){};
 
 		
-		//Applies the filter to all cells that are within the filter's domain.
+		//Applies the filter to all cells that are within the filter's sequence's domain.
 		virtual void operator()() = 0;
 	protected:
+		/**
+		 *  Filters should read from input vector and write to output vector.
+		 *  Both vectors use the same indexing by default. 
+		 *	All unmodified cells of the output vector are implicitly copied from their respective input counterpart,
+		 *	i.e it is not mandatory to have any output.
+		 */
 		std::vector<coupling::datastructures::MacroscopicCell<dim>* > _inputCells; 	
 		std::vector<coupling::datastructures::MacroscopicCell<dim>* > _outputCells;
-		std::vector<tarch::la::Vector<dim,unsigned int>> _cellIndices; //indices for both cell vectors are identical
+		std::vector<tarch::la::Vector<dim,unsigned int>> _cellIndices;
 };
