53519b2fac48324d9351d65cebe3949c59a7cefb
Felix Maurer <felix.maurer@informatik.uni-hamburg.de>
2020-08-02 15:23:41 +0200

    FilterInterfaces select what to filter based on XML. Gauss works properly.

--- coupling/filtering/filters/Gauss.h
+++ coupling/filtering/filters/Gauss.h
@@ -7,7 +7,7 @@
 #include <string>
 #include <vector>
 
-//#define DEBUG_GAUSS
+#define DEBUG_GAUSS
 #include "coupling/filtering/FilterInterface.h"
 
 namespace coupling {
@@ -29,39 +29,38 @@ class coupling::Gauss : public coupling::FilterInterface<dim>{
         Gauss(  const std::vector<coupling::datastructures::MacroscopicCell<dim> *>& inputCellVector,
 				const std::vector<coupling::datastructures::MacroscopicCell<dim> *>& outputCellVector,
 				const std::vector<tarch::la::Vector<dim, unsigned int>> cellIndices, //Use local indexing! (starting at (0,...,0))
-				unsigned int d,
-				const bool uses[7]):
-				
-				
-				coupling::FilterInterface<dim>(inputCellVector, outputCellVector, cellIndices),
-				_dim(d),
-				_lastIndex(coupling::FilterInterface<dim>::_cellIndices.back()),
-				_useMicroMass(uses[0]),
-			 	_useMicroMomentum(uses[1]),
-				_useMacroMass(uses[2]),
-				_useMacroMomentum(uses[3]),
-				_usePotEnergy(uses[4]),
-				_useVelocity(uses[5]),
-				_useTemperature(uses[6])
+				bool filteredValues[7],
+				unsigned int dimension,
+				const char* extrapolationStrategy):
+				coupling::FilterInterface<dim>(inputCellVector, outputCellVector, cellIndices, filteredValues),
+				_dim(dimension),
+				_lastIndex(coupling::FilterInterface<dim>::_cellIndices.back())
+				//TODO: function pointers
 		{
-			for(unsigned int d = 0; d < dim; d++)if(coupling::FilterInterface<dim>::_cellIndices.back()[d] < 2){
-				std::cout << "ERROR: GAUSS: Input domain has to be " << dim << " dimensional." << std::endl;
+			if(coupling::FilterInterface<dim>::_cellIndices.back()[_dim] < 2){
+				std::cout << "ERROR: GAUSS: Invalid input domain." << std::endl;
+				exit(EXIT_FAILURE);
 			}
+
+			if(extrapolationStrategy == nullptr || std::strcmp(extrapolationStrategy, "none") == 0) _extrapolationStrategy = 0;
+			else if(std::strcmp(extrapolationStrategy, "linear") == 0) _extrapolationStrategy = 1;
+			else{
+				#ifdef DEBUG_GAUSS
+				std::cout << "ERROR: GAUSS: Unknown extrapolation strategy:" << extrapolationStrategy << std::endl;
+				#endif
+				exit(EXIT_FAILURE);
+			}
+
         	#ifdef DEBUG_GAUSS
-			std::cout << "		GAUSS: Created Gaussian filter. It will filter:" << std::endl;
-			if(_useMicroMass) std::cout << "		- Microscopic mass" << std::endl;
-			if(_useMicroMomentum) std::cout << "		- Microscopic momentum" << std::endl;
-			if(_useMacroMass) std::cout << "		- Macroscopic mass" << std::endl;
-			if(_useMacroMomentum) std::cout << "		- Macroscopic momentum" << std::endl;
-			if(_usePotEnergy) std::cout << "		- Potential energy" << std::endl;
-			if(_useVelocity) std::cout << "		- Velocity" << std::endl;
-			if(_useTemperature) std::cout << "		- Temperature" << std::endl;
+			std::cout << "		GAUSS (Dim: " << _dim << ": Created Gaussian filter." << std::endl;
+			if(_extrapolationStrategy == 0) std::cout << "		It will not use extrapolation." << std::endl;
+			if(_extrapolationStrategy == 1) std::cout << "		It will use linear extrapolation." << std::endl;
        		#endif
         }
 
         ~Gauss(){
         	#ifdef DEBUG_GAUSS
-            std::cout << "		GAUSS: Gaussian filter deconstructed" << std::endl;
+            std::cout << "		GAUSS (Dim: " << _dim << "): Gaussian filter deconstructed" << std::endl;
         	#endif
         }
 
@@ -70,23 +69,21 @@ class coupling::Gauss : public coupling::FilterInterface<dim>{
 	private:
 		//on which axis this filter operates. 0 <= _dim <= dim
 		unsigned int _dim;
+		
+		/**
+		 * Determines how to apply filter to border cells:
+		 * 0 = only use existing cells and increase their weight accordingly
+		 * 1 = linear extrapolation //TODO
+		 */
+		unsigned int _extrapolationStrategy;
 
 		//returns the cell that's above the cell at index on the d-axis
-		unsigned int getLowerCellIndex(unsigned int index, unsigned int d);
+		unsigned int getIndexBelow(unsigned int index, unsigned int d);
 		//returns the cell that's below the cell at index on the d-axis
-		unsigned int getUpperCellIndex(unsigned int index, unsigned int d);
+		unsigned int getIndexAbove(unsigned int index, unsigned int d);
 
 		//TODO: Move to interface?
 		tarch::la::Vector<dim, unsigned int> _lastIndex;
-
-		//TODO: Move to interface?
-		bool _useMicroMass;
-		bool _useMicroMomentum;
-		bool _useMacroMass;
-		bool _useMacroMomentum;
-		bool _usePotEnergy;
-		bool _useVelocity;
-		bool _useTemperature;
 };
 
 //include implementation of header
