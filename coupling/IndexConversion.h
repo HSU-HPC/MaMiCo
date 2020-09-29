3156f6c1621e7b071d959392c359b1b49d70e3a2
Felix Maurer <felix.maurer@informatik.uni-hamburg.de>
2020-09-29 21:22:55 +0200

    FP now uses IC's local indexing as intended.

--- coupling/IndexConversion.h
+++ coupling/IndexConversion.h
@@ -80,15 +80,18 @@ class coupling::IndexConversion: private tarch::utils::Uncopyable {
      */
     tarch::la::Vector<dim,unsigned int> getGlobalInnerVectorCellIndex(unsigned int globalCellIndex) const;
 
-	//TODO: unused function
-	/** Translates index vector of inner cell to make it usable in global context*/
-	tarch::la::Vector<dim,unsigned int> convertInnerVectorCellIndexToGlobal(tarch::la::Vector<dim,unsigned int> cellIndexVectorMDContext) const;
-
     /** returns the local vector cell index from a linearised index. This function just performs the inverse operation to
      *  getLocalCellIndex(vectorIndex), i.e. getLocalVectorCellIndex(getLocalCellIndex(somevector) ) should again return
      *  somevector.
      */
     tarch::la::Vector<dim,unsigned int> getLocalVectorCellIndex(unsigned int localCellIndex) const;
+
+	/** Does the same as getLocalVectorCellIndex() but 
+	 * 		* non-inner cells (i.e. outside of the MD domain) are converted to MAX_INT
+	 * 		* (0, ... , 0) is the first cell in the MD domain etc.
+     */
+    tarch::la::Vector<dim,unsigned int> getLocalInnerVectorCellIndex(unsigned int localCellIndex) const;
+
 
     /** converts a local vector cell index into a global vector cell index */
     tarch::la::Vector<dim,unsigned int> convertLocalToGlobalVectorCellIndex(
