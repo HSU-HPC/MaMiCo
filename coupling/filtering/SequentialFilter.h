0865db008fda12c2a6b9d6f6b2b9b507c103d124
Felix Maurer <felix.maurer@informatik.uni-hamburg.de>
2020-11-10 21:56:49 +0100

    SequentialFilter uses ICM2M for cross-rank indexing. Integration of ICM2M for FilterPipeline/-Sequence

--- coupling/filtering/SequentialFilter.h
+++ coupling/filtering/SequentialFilter.h
@@ -4,6 +4,7 @@
 
 #pragma once
 #include "coupling/filtering/interfaces/FilterInterface.h"
+#include "coupling/IndexConversionMD2Macro.h"
 #include <mpi.h>
 
 
@@ -19,6 +20,7 @@ namespace coupling{
 
 
 /*
+ * TODO: update
  * Implementation of FilterInterface.h for filters which operate (optionally or mandatorily) in a sequential manner, i.e. process data on one master rank.
  * For such filters, operator()() will
  * 		- contribute to one dedicated processing rank: by calling contribute()
@@ -35,48 +37,15 @@ namespace coupling{
  * @Author Felix Maurer
  */
 
+
 template <unsigned int dim>
 class coupling::SequentialFilter : public coupling::FilterInterface<dim> {
 	public:
 		SequentialFilter(
 					coupling::FilterInterface<dim>* filter,
-					const coupling::IndexConversion<dim>* ic = nullptr, //null if run locally i.e. parallel
+					const coupling::IndexConversionMD2Macro<dim>* ic = nullptr, //null if run locally i.e. parallel
 					const MPI_Comm comm = MPI_COMM_WORLD //null if run locally i.e. parallel TODO: in case of multimd, this goes very wrong
-		):
-			coupling::FilterInterface<dim>("SEQUENTIALIZED_FILTER"),
-			_filter(filter),
-			_ic(ic),
-			_comm(comm),
-			_processingRank(0), //TODO: case: multimd
-			_cellsPerRank(_filter->getSize()),
-			_firstIteration(true)
-		{
-			#ifdef DEBUG_SEQ_FILTER
-			std::cout << "		SEQFILTER: Now initializing sequentialized Filter of type: " << _filter->getType() <<std::endl;
-			#endif
-
-			//case: sequential
-			if(_ic) {
-				MPI_Comm_size(comm, &_commSize);	
-
-				//allocate global cell data structures for processing rank only
-				if( _processingRank == (int) _ic->getThisRank()) {
-					_outputCells_Local = _filter->getOutputCells(); //filter's output cell pointers will be overwritten, but we need this information in operator()
-
-					for(int c = 0; c < _cellsPerRank * _commSize; c++) {
-						_inputCells_Global.push_back(new coupling::datastructures::MacroscopicCell<dim>());
-						_outputCells_Global.push_back(new coupling::datastructures::MacroscopicCell<dim>());
-					}
-				}
-			}
-			//case: not sequential
-			else {
-				_commSize = 0;
-				#ifdef DEBUG_SEQ_FILTER
-				std::cout << "		SEQFILTER: It will not run sequentially." << std::endl;
-				#endif
-			}
-		}
+		);
 
 		~SequentialFilter() {
 			delete _filter;
@@ -88,8 +57,8 @@ class coupling::SequentialFilter : public coupling::FilterInterface<dim> {
 		 * Implements FilterInterface's requirement of having a ()-operand defined.
 		 */
 		virtual void operator()(){
-			if(_ic) {
-				if(_processingRank == (int) _ic->getThisRank()) {
+			if((*_ic)()) {
+				if(_processingRank == (int) (*_ic)()->getThisRank()) {
 					contribute();
 					process(FILTER_SEQUENTIAL); 				
 				}
@@ -99,7 +68,7 @@ class coupling::SequentialFilter : public coupling::FilterInterface<dim> {
 				MPI_Scatter(_sendbuf.data(), _cellsPerRank, MPI_DOUBLE, _recvbuf.data(), _cellsPerRank * _commSize, MPI_DOUBLE, _processingRank, _comm); 
 
 				//Read output data from buffer
-				if(_processingRank == (int) _ic->getThisRank()) applyBufferToMacroscopicCells(_recvbuf, _outputCells_Local);
+				if(_processingRank == (int) (*_ic)()->getThisRank()) applyBufferToMacroscopicCells(_recvbuf, _outputCells_Local);
 				else applyBufferToMacroscopicCells(_recvbuf, _filter->getOutputCells());
 			}
 			else process(FILTER_PARALLEL);
@@ -110,52 +79,7 @@ class coupling::SequentialFilter : public coupling::FilterInterface<dim> {
 		/*
 		 * When sequentialized, all ranks call this function.
 		 */
-		virtual void contribute() {
-			//communicate indices during first iteration
-			if(_firstIteration == true) {
-				std::vector<unsigned int> indexbuf;
-				std::vector<unsigned int> indexrecvbuf; //accessed only by processing rank
-
-				//TODO: performance can be improved
-				for(int i_local = 0; i_local < _cellsPerRank; i_local++){
-					//we dont actually use global cell indices, this is just a way to compress data
-					indexbuf.push_back(_ic->getGlobalCellIndex(_filter->getCellIndices()[i_local]));
-				}
-				if( _processingRank == (int) _ic->getThisRank()) indexrecvbuf.resize(_cellsPerRank * _commSize);
-
-				//gather all linear indices at processing rank
-				MPI_Gather(indexbuf.data(), _cellsPerRank, MPI_UNSIGNED, indexrecvbuf.data(), _cellsPerRank * _commSize, MPI_UNSIGNED, _processingRank, _comm); 
-
-				//Parse to _cellIndices_Global
-				if( _processingRank == (int) _ic->getThisRank())
-				for(int i_global = 0; i_global < (int) indexrecvbuf.size(); i_global++)
-					//revert compression to linear index
-					_cellIndices_Global.push_back(_ic->getGlobalVectorCellIndex(indexrecvbuf[i_global]));
-				
-				//for(auto index : _cellIndices_Global) std::cout << index << std::endl;
-
-				_firstIteration = false;
-			}
-
-			//construct MacroscopicCell buffer from input cell vector
-			macroscopicCellsToBuffer(_sendbuf, _filter->getInputCells());
-
-			#ifdef DEBUG_SEQ_FILTER
-			std::cout << "		SEQFILTER: Sending buffer of size: " << _sendbuf.size() << std::endl;
-			#endif
-
-			//TODO: unneccesary to do this in each iteration
-			//allocate on processing rank
-			if(_processingRank == (int) _ic->getThisRank()) _recvbuf.resize(_sendbuf.size()*_commSize);
-	
-			//send to processing rank 
-			MPI_Gather(_sendbuf.data(), _sendbuf.size(), MPI_DOUBLE, _recvbuf.data(), _sendbuf.size() * _commSize, MPI_DOUBLE, _processingRank, _comm); 
-
-			#ifdef DEBUG_SEQ_FILTER
-			if(_processingRank == (int) _ic->getThisRank()) std::cout << "		SEQFILTER: Receiving buffer of size: " << _recvbuf.size() << std::endl;
-			#endif
-		}
-
+		virtual void contribute();
 
 		/*
 		 * When sequentialized, only the processing rank calls this function. It acts as a wrapper of _filter's operator() member function.
@@ -166,9 +90,6 @@ class coupling::SequentialFilter : public coupling::FilterInterface<dim> {
 
 				//write all gathered cells to inputCells_Global.
 				applyBufferToMacroscopicCells(_recvbuf, _inputCells_Global);
-
-				//Get global indexing right
-				//TODO for _commSize > 1
 
 				//Apply _filter
 				_filter->updateCellData(_inputCells_Global, _outputCells_Global, _cellIndices_Global);
@@ -182,47 +103,15 @@ class coupling::SequentialFilter : public coupling::FilterInterface<dim> {
 			}
 		}
 
-		void macroscopicCellsToBuffer(std::vector<double>& buf, const std::vector<coupling::datastructures::MacroscopicCell<dim> *>& cells){
-			if(cells.empty()) std::cout << "		SEQFILTER: MCTB: Warning: empty cell vector.";
+		void macroscopicCellsToBuffer(std::vector<double>& buf, const std::vector<coupling::datastructures::MacroscopicCell<dim> *>& cells);
 
-			//copy cell data to buffer
-			for(auto cell : cells) {
-				buf.push_back(cell->getMicroscopicMass()); //std::cout << buf.back() << " ";
-				for (unsigned d = 0; d < dim; d++) { buf.push_back(cell->getMicroscopicMomentum()[d]); /*std::cout << buf.back() << " ";*/ } 
-				buf.push_back(cell->getMacroscopicMass()); //std::cout << buf.back() << " ";
-				for (unsigned d = 0; d < dim; d++) { buf.push_back(cell->getMacroscopicMomentum()[d]); /*std::cout << buf.back() << " ";*/ } 
-				buf.push_back(cell->getPotentialEnergy()); //std::cout << buf.back() << " ";
-				for (unsigned d = 0; d < dim; d++) { buf.push_back(cell->getCurrentVelocity()[d]); /*std::cout << buf.back() << " ";*/ } 
-				buf.push_back(cell->getTemperature()); //std::cout << buf.back() << " ";
-				//std::cout << std::endl;
-			}
-		}	
-
-		void applyBufferToMacroscopicCells(std::vector<double>& buf, const std::vector<coupling::datastructures::MacroscopicCell<dim> *>& cells) {
-
-			std::cout << "		SEQFILTER: ABTMC: Buffer size: " << buf.size() << " Cell vector size: " << cells.size() << std::endl;
-			//TODO
-			if(buf.size() != cells.size() * (4+3*dim) ) throw std::runtime_error("Buffer and cell vector size must be the same!"); //TODO
-
-			//copy buffer data to cells
-			tarch::la::Vector<dim, double> mvec_buf;
-			unsigned int i = 0;
-			for(unsigned int c = 0; c < cells.size(); c++) {
-				cells[c]->setMicroscopicMass(buf[i]); i++;
-				for(unsigned int d = 0; d < dim; d++) { mvec_buf[d] = buf[i]; i++; } cells[c]->setMicroscopicMomentum(mvec_buf);
-				cells[c]->setMacroscopicMass(buf[i]); i++;
-				for(unsigned int d = 0; d < dim; d++) { mvec_buf[d] = buf[i]; i++; } cells[c]->setMacroscopicMomentum(mvec_buf);
-				cells[c]->setPotentialEnergy(buf[i]); i++;
-				for(unsigned int d = 0; d < dim; d++) { mvec_buf[d] = buf[i]; i++; } cells[c]->setCurrentVelocity(mvec_buf);
-				cells[c]->setTemperature(buf[i]); i++;
-			}
-		}
+		void applyBufferToMacroscopicCells(std::vector<double>& buf, const std::vector<coupling::datastructures::MacroscopicCell<dim> *>& cells);
 
 		//The sequentialized Filter
 		coupling::FilterInterface<dim>* _filter;
 
 		//Null if run locally.
-		const coupling::IndexConversion<dim>* _ic;	
+		const coupling::IndexConversionMD2Macro<dim>* _ic;	
 
 		//Possibility: Individual MPI communicator per globalized filter. Probably either nullptr or MPI_COMM_WORLD for now.
 		const MPI_Comm _comm;
@@ -245,9 +134,12 @@ class coupling::SequentialFilter : public coupling::FilterInterface<dim> {
 		bool _firstIteration;
 };
 
+#include "SequentialFilter.cpph"
+
 
 /*
  * TODO
  * what if not all ranks have exactly the same amounts of cell
  * testing
+ * divite into header and implementation
  */
