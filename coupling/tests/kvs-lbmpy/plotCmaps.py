601ced0b854d35d3caa8cf49d9f7d6b132c29ae8
Piet Jarmatz <jarmatz@hsu-hh.de>
2021-02-12 07:37:31 +0100

    plotCmaps axes label fix

--- coupling/tests/kvs-lbmpy/plotCmaps.py
+++ coupling/tests/kvs-lbmpy/plotCmaps.py
@@ -23,7 +23,11 @@ for i in range(5):
 	pc = ax[i].pcolor(a, vmin=0.85, vmax=1.15)
 	ax[i].set_title(titles[i])
 	ax[i].set_xlabel('cell x')
-	ax[i].set_ylabel('time t')
+	ax[i].set_ylabel('time t (coupling cycles)')
+	ax[i].set_xticks([0.5,1.5,2.5,3.5])
+	ax[i].set_xticklabels(['0','1','2','3'])
+	ax[i].set_yticks([0.5,1.5,2.5,3.5,4.5,5.5,6.5,7.5])
+	ax[i].set_yticklabels(['0','1','2','3','4','5','6','7'])
 
 fig.subplots_adjust(right=0.88, wspace=0.32)
 cbar_ax = fig.add_axes([0.90, 0.15, 0.01, 0.7]) #left, bottom, width, height
