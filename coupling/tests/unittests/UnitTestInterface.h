3a9aa6cca929ee18e8f9b86fe39551ca48a43f30
Felix Maurer <felix.maurer@informatik.uni-hamburg.de>
2021-03-01 22:38:49 +0100

    Reduced code duplication in test funcs. Now parallelizable.

--- coupling/tests/unittests/UnitTestInterface.h
+++ coupling/tests/unittests/UnitTestInterface.h
@@ -20,27 +20,66 @@
 
 namespace testing { namespace ut {
 		class UnitTestInterface;
+		template<class T>
+		class UnitTestImpl;
 }}
 
+//TODO: A LOT OF COMMENTS
+
+//EVERYTHING NOT TEMPLATE DEPENDANT
 class testing::ut::UnitTestInterface {
 	public:
-		UnitTestInterface(std::string classIdentifier):
-		_classIdentifier(classIdentifier)
+		UnitTestInterface(std::string classIdentifier): _classIdentifier(classIdentifier) {}
+		virtual void runAllTests() {}
+		virtual void runTest() {}
+
+		//used when handling strings
+		std::string getClassIdentifier() const { return _classIdentifier; }
+
+		//used when outputting identifer
+		std::string getClassIdentifier_pretty() const { return std::string("\x1B[36m").append(_classIdentifier).append("\x1B[0m"); }
+
+	protected:
+		//prefix for all output of classes deriving this
+		virtual std::string PRINT_PREFIX() const {
+			return std::string("UnitTest(").append(getClassIdentifier_pretty()).append("): ");
+		}
+
+		/*
+		 * Each UnitTest must provide a MockService of the class it is testing.
+		 * This should be done during instanciation of the UnitTest.
+		 */
+		testing::ut::MockService* _mockService;
+
+		/*
+		 * This is typeid(T).name() where T is the tested class
+		 */
+		std::string _classIdentifier;
+
+};
+
+//EVERYTHING TEMPLATE DEPENDANT
+template<class T>
+class testing::ut::UnitTestImpl : public testing::ut::UnitTestInterface {
+	public:
+		UnitTestImpl():
+		UnitTestInterface(typeid(T).name())
 		{	
 			#ifdef DEBUG_UT
-				std::cout << PRINT_PREFIX() << "Constructed." << std::endl;
+				std::cout << this->PRINT_PREFIX() << "Constructed." << std::endl;
 			#endif
 		}
 
 
-		virtual ~UnitTestInterface() {
+		virtual ~UnitTestImpl() {
 			//delete all functions pointers stored in testFuncs
-			for(auto func : _testFuncs) delete get<0>(func);
+			for(auto func : _testFuncs) delete std::get<0>(func);
 			#ifdef DEBUG_UT
-				std::cout << PRINT_PREFIX() << "Deconstructed." << std::endl;
+				std::cout << this->PRINT_PREFIX() << "Deconstructed." << std::endl;
 			#endif
 		}
 
+		//TODO: move to UTInterface?
 		//Executes all unit test functions of this UT
 		virtual void runAllTests() {
 			for(unsigned int tf_i = 0; tf_i < _testFuncs.size(); tf_i++) 
@@ -57,7 +96,17 @@ class testing::ut::UnitTestInterface {
 			try {
 				//TODO: time measurement
 				//Gets the first part of the testFunc tuple, i.e. a function pointer, and then dereferences it
-				(*std::get<0>(_testFuncs[testFunc_index]))();
+				auto testFunctionPtr = std::get<0>(_testFuncs[testFunc_index]);
+
+				//for all possible mocks of tested type T...
+				for(auto mock : this->_mockService->getMocksCopy<T>()) {
+					//...execute the function
+					(*testFunctionPtr)(mock);
+				}
+
+				#ifdef DEBUG_UT
+					std::cout << this->PRINT_PREFIX() << std::get<1>(_testFuncs[testFunc_index]) << ": \x1B[032m Success! \x1B[0m" << std::endl;
+				#endif
 			}
 			catch (std::exception& e) {
 				//Enrich exception info with idenfier of failed function (i.e second part of testFunc tuple)
@@ -65,39 +114,17 @@ class testing::ut::UnitTestInterface {
 			}
 		}
 
-		//used when handling strings
-		std::string getClassIdentifier() const { return _classIdentifier; }
-
-		//used when outputting identifer
-		std::string getClassIdentifier_pretty() const { return std::string("\x1B[36m").append(_classIdentifier).append("\x1B[0m"); }
-
-	protected:
-		
-		//prefix for all output of classes deriving this
-		virtual std::string PRINT_PREFIX() const {
-			return std::string("UnitTest(").append(getClassIdentifier_pretty()).append("): ");
-		}
-
+	protected:	
 		/*
 		 * List of tuples: 
 		 * (0) 	function pointers matching the following interface:
-		 * 		(void) -> void
+		 * 		(T&) -> void
 		 *
 		 * 		Expected to throw exception if assertions fail.
 		 * 		That exception then gets passed to the UTS, cf. this->run(All)Tests()
 		 * (1)	Identifer string for that function. Usually its source code name.
 		 */
-		std::vector<std::tuple<std::function<void ()>*, std::string>> _testFuncs;
+		std::vector<std::tuple<std::function<void (T&)>*, std::string>> _testFuncs;
 
-		/*
-		 * Each UnitTest must provide a MockService of the class it is testing.
-		 * This should be done during instanciation of the UnitTest.
-		 */
-		testing::ut::MockService* _mockService;
-
-		/*
-		 * This should be typeid(T).name() where T is the tested class
-		 */
-		std::string _classIdentifier;
 };
 
