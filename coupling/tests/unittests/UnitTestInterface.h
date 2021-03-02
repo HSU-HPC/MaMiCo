17af3d43b6d4120403f261123a77aa5334fc9356
Felix Maurer <felix.maurer@informatik.uni-hamburg.de>
2021-03-02 21:27:08 +0100

    Split UnitTestImpl and UnitTestInterface into seperate source files. Added parallelization over mock domain.

--- coupling/tests/unittests/UnitTestInterface.h
+++ coupling/tests/unittests/UnitTestInterface.h
@@ -4,11 +4,7 @@
 #pragma once
 
 #include<vector>
-#include<tuple>
 #include<iostream> 
-#include<functional>
-#include<exception>
-#include<stdexcept>
 
 //#define DEBUG_UT
 
@@ -20,13 +16,8 @@
 
 namespace testing { namespace ut {
 		class UnitTestInterface;
-		template<class T>
-		class UnitTestImpl;
 }}
 
-//TODO: Split into two source files
-
-//EVERYTHING NOT TEMPLATE DEPENDANT
 class testing::ut::UnitTestInterface {
 	public:
 		UnitTestInterface(std::string classIdentifier, int rank = 0, int comm_size = 0): 
@@ -35,6 +26,11 @@ class testing::ut::UnitTestInterface {
 			_comm_size(comm_size)
 		{}
 
+		/*
+		 * These two functions are implemented by the expanded interface UnitTestImpl.
+		 * All unit tests should implement not only UnitTestInterface, but also UnitTestImpl.
+		 */
+		//TODO: Exception when these unimplemented functions get called.
 		virtual void runAllTests() {}
 		virtual void runTest() {}
 
@@ -69,72 +65,7 @@ class testing::ut::UnitTestInterface {
 
 };
 
-//EVERYTHING TEMPLATE DEPENDANT
-template<class T>
-class testing::ut::UnitTestImpl : public testing::ut::UnitTestInterface {
-	public:
-		UnitTestImpl(int rank = 0, int comm_size = 0):
-		UnitTestInterface(typeid(T).name(), rank, comm_size)
-		{	
-			#ifdef DEBUG_UT
-				std::cout << this->PRINT_PREFIX() << "Constructed." << std::endl;
-			#endif
-		}
-
-
-		virtual ~UnitTestImpl() {
-			//delete all functions pointers stored in testFuncs
-			for(auto func : _testFuncs) delete std::get<0>(func);
-			#ifdef DEBUG_UT
-				std::cout << this->PRINT_PREFIX() << "Deconstructed." << std::endl;
-			#endif
-		}
-
-		//TODO: move to UTInterface?
-		//Executes all unit test functions of this UT
-		virtual void runAllTests() {
-			for(unsigned int tf_i = 0; tf_i < _testFuncs.size(); tf_i++) 
-				try {
-					runTest(tf_i);
-				}
-				catch (...) {
-					std::rethrow_exception(std::current_exception());
-				}
-		}
-
-		//Executes a single test.
-		virtual void runTest(unsigned int testFunc_index) {
-			try {
-				//TODO: time measurement
-				//Gets the first part of the testFunc tuple, i.e. a function pointer, and then dereferences it
-				auto testFunctionPtr = std::get<0>(_testFuncs[testFunc_index]);
-
-				//for all possible mocks of tested type T...
-				for(auto mock : this->_mockService->getMocksCopy<T>()) {
-					//...execute the function
-					(*testFunctionPtr)(mock);
-				}
-
-				//TODO: MPI Barrier here. Output only on master rank.
-				std::cout << this->PRINT_PREFIX() << std::get<1>(_testFuncs[testFunc_index]) << ": \x1B[032m Success! \x1B[0m" << std::endl;
-			}
-			catch (std::exception& e) {
-				//Enrich exception info with idenfier of failed function (i.e second part of testFunc tuple)
-				throw std::runtime_error(std::string("\x1B[31m").append(std::get<1>(_testFuncs[testFunc_index])).append(": ").append(e.what()).append("\x1B[0m"));
-			}
-		}
-
-	protected:	
-		/*
-		 * List of tuples: 
-		 * (0) 	function pointers matching the following interface:
-		 * 		(T&) -> void
-		 *
-		 * 		Expected to throw exception if assertions fail.
-		 * 		That exception then gets passed to the UTS, cf. this->run(All)Tests()
-		 * (1)	Identifer string for that function. Usually its source code name.
-		 */
-		std::vector<std::tuple<std::function<void (T&)>*, std::string>> _testFuncs;
-
-};
-
+/*
+ * The class UnitTestImpl inherits from this and introduces template dependant runTest functions.
+ */
+#include"UnitTestImpl.h"
