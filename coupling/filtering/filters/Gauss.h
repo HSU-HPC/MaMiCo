e5808b00d703a31b4d930b5a695105e42ec28693
Felix Maurer <felix.maurer@informatik.uni-hamburg.de>
2021-05-25 23:17:12 +0200

    refactor: refactor of gauss cpp filter, wip: gaussian distribution/kernel calculation

--- coupling/filtering/filters/Gauss.h
+++ coupling/filtering/filters/Gauss.h
@@ -6,6 +6,7 @@
 
 #include <string>
 #include <vector>
+#include <cmath>
 
 //#define DEBUG_GAUSS
 #include "coupling/filtering/interfaces/FilterInterface.h"
@@ -15,13 +16,8 @@ namespace coupling {
     class Gauss;
 }
 
-//DISCLAIMER:
-//This filter always uses the following parameters for kernel generation:
-#define GAUSS_SIGMA 1
-#define GAUSS_KERNEL_SIZE 3
-//Which implies:
-#define GAUSS_OUTER_WEIGHT 0.27901
-#define GAUSS_INNER_WEIGHT (1- (GAUSS_OUTER_WEIGHT * 2))
+//Define kernel radius. e.g. radius = 1 means kernel size of 3
+#define GAUSS_KERNEL_RADIUS 1
 
 /*
  * Implements a gaussian filter. Limited to what is listed in the disclaimer above. 
@@ -31,32 +27,50 @@ namespace coupling {
  */
 template<unsigned int dim>
 class coupling::Gauss : public coupling::FilterInterface<dim>{
+	using coupling::FilterInterface<dim>::_inputCells;
+	using coupling::FilterInterface<dim>::_outputCells;
+	using coupling::FilterInterface<dim>::_cellIndices;
+	using coupling::FilterInterface<dim>::_scalarGetters;
+	using coupling::FilterInterface<dim>::_vectorGetters;
+	using coupling::FilterInterface<dim>::_scalarSetters;
+	using coupling::FilterInterface<dim>::_vectorSetters;
+
     public:
         Gauss(  const std::vector<coupling::datastructures::MacroscopicCell<dim> *>& inputCellVector,
 				const std::vector<coupling::datastructures::MacroscopicCell<dim> *>& outputCellVector,
 				const std::vector<tarch::la::Vector<dim, unsigned int>> cellIndices, //Use local indexing! (starting at (0,...,0))
 				const std::array<bool, 7> filteredValues,
 				unsigned int dimension,
+				unsigned int sigma,
 				const char* extrapolationStrategy):
 				coupling::FilterInterface<dim>(inputCellVector, outputCellVector, cellIndices, filteredValues, "GAUSS"),
 				_dim(dimension),
-				_lastIndex(coupling::FilterInterface<dim>::_cellIndices.back())
+				_sigma(sigma),
+				_lastIndex(coupling::FilterInterface<dim>::_cellIndices.back()),
+				_kernel(generateKernel())
 		{
-			//TODO
 			std::cout << "WARNING: You're using a GAUSS-Filter. As this filter has not been tested thoroughly, caution is advised!" << std::endl;
 
-			if(coupling::FilterInterface<dim>::_cellIndices.back()[_dim] < 2){
-				std::cout << "ERROR: GAUSS: Invalid input domain." << std::endl;
-				exit(EXIT_FAILURE);
-			}
+			//TODO
+			if(GAUSS_KERNEL_RADIUS != 1) 
+				throw std::runtime_error("ERROR: GAUSS: Kernel radius != 1 currently not supported.");
+
+			//TODO
+			if(sigma != 1) 
+				throw std::runtime_error("ERROR: GAUSS: sigma != 1 currently not supported.");
+
+			//Overwrite kernel with hardcoded weights TODO: fix kernel generation, then remove this
+			_kernel = {0.27901, 0.44198, 0.27901};
+
+
+			if(coupling::FilterInterface<dim>::_cellIndices.back()[_dim] < 2)
+				throw std::runtime_error("ERROR: GAUSS: Invalid input domain.");
 
 			if(extrapolationStrategy == nullptr || std::strcmp(extrapolationStrategy, "none") == 0) _extrapolationStrategy = 0;
 			else if(std::strcmp(extrapolationStrategy, "linear") == 0) _extrapolationStrategy = 1;
-			else{
-				#ifdef DEBUG_GAUSS
-				std::cout << "ERROR: GAUSS: Unknown extrapolation strategy:" << extrapolationStrategy << std::endl;
-				#endif
-				exit(EXIT_FAILURE);
+			else {
+				std::cout << "Extrapolation strategy: " << extrapolationStrategy << std::endl;
+				throw std::runtime_error("ERROR: GAUSS: Unknown extrapolation strategy.");
 			}
 
         	#ifdef DEBUG_GAUSS
@@ -71,26 +85,38 @@ class coupling::Gauss : public coupling::FilterInterface<dim>{
             std::cout << "		GAUSS (Dim: " << _dim << "): Gaussian filter deconstructed" << std::endl;
         	#endif
         }
-
      
 	    void operator()();
 	private:
-		//on which axis this filter operates. 0 <= _dim <= dim
-		unsigned int _dim;
-		
-		/**
-		 * Determines how to apply filter to border cells:
-		 * 0 = only use existing cells and increase their weight accordingly
-		 * 1 = linear extrapolation
-		 */
-		unsigned int _extrapolationStrategy;
+		std::array<double, 1+2*GAUSS_KERNEL_RADIUS> generateKernel();
+
+		double gaussianDensityFunction(int x);
 
 		//returns the cell that's above the cell at index on the d-axis
 		unsigned int getIndexBelow(unsigned int index, unsigned int d);
 		//returns the cell that's below the cell at index on the d-axis
 		unsigned int getIndexAbove(unsigned int index, unsigned int d);
 
+
+		//on which axis this filter operates. 0 <= _dim <= dim
+		unsigned int _dim;
+
+		//standard deviation used
+		unsigned int _sigma;
+
 		tarch::la::Vector<dim, unsigned int> _lastIndex;
+
+		std::array<double, 1+2*GAUSS_KERNEL_RADIUS> _kernel;
+		
+		/**
+		 * Determines how to apply filter to border cells:
+		 * 0 = only use existing cells and increase their weight accordingly
+		 * 1 = linear extrapolation
+		 * TODO: use enum
+		 */
+		unsigned int _extrapolationStrategy;
+
+
 };
 
 //include implementation of header
