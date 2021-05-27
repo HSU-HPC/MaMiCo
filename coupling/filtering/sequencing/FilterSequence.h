eaeaeb77027bd91aee7f3a194b3a7ed81c889898
Felix Maurer <felix.maurer@informatik.uni-hamburg.de>
2021-05-27 22:50:47 +0200

    fix: compilation errors after merge. disabled POD due to eigen3 errors.

--- coupling/filtering/sequencing/FilterSequence.h
+++ coupling/filtering/sequencing/FilterSequence.h
@@ -14,7 +14,7 @@
 #include "coupling/filtering/filters/WriteToFile.h"
 #include "coupling/filtering/filters/ReadFromFile.h"
 #include "coupling/filtering/filters/Gauss.h"
-#include "coupling/filtering/filters/POD.h"
+//#include "coupling/filtering/filters/POD.h" TODO: fix eigen error
 #include "coupling/filtering/filters/NLM.h"
 #include "coupling/filtering/filters/Strouhal.h"
 #include "coupling/filtering/filters/FilterFromFunction.h"
