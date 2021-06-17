4fa6e4f7743fd7c0fe79467f2927e0071d6aab9a
amartyads <amartyads@gmail.com>
2021-06-17 14:13:44 +0200

    new blank files 2

---
+++ coupling/interface/impl/ls1/LS1MDSolverInterface.h
@@ -0,0 +1,15 @@
+#ifndef LS1_MD_SOLVER_INTERFACE_H_
+#define LS1_MD_SOLVER_INTERFACE_H_
+
+#include "coupling/interface/MDSolverInterface.h"
+
+namespace coupling
+{
+    namespace interface
+    {
+        class LS1MDSolverInterface;
+    }
+}
+
+
+#endif
---
+++ coupling/interface/impl/ls1/LS1MoleculeIterator.h
@@ -0,0 +1,15 @@
+#ifndef LS1_MOLECULE_ITERATOR_H_
+#define LS1_MOLECULE_ITERATOR_H_
+
+#include "coupling/interface/MoleculeIterator.h"
+
+namespace coupling
+{
+    namespace interface
+    {
+        class LS1MoleculeIterator;
+    }
+}
+
+
+#endif
