e48c9358a7fcd3b1551f7e422790cdf8c9c318b8
Felix Maurer <felix.maurer@informatik.uni-hamburg.de>
2021-08-26 18:42:08 +0200

    refactor: remove forward decl in Operation.h

--- coupling/indexing/Operations.h
+++ coupling/indexing/Operations.h
@@ -13,38 +13,31 @@ namespace coupling {
 
 		template<unsigned int dim, IndexType idx_T>
 		CellIndex<dim, {false, idx_T.local, idx_T.md2macro, idx_T.noGhost}>
-		convertToScalar(CellIndex<dim, idx_T> index);
+		convertToScalar(CellIndex<dim, idx_T> index) {
+			if constexpr(idx_T.vector) {
+				//TODO: dummy implementation, copy actual behaviour from IndexConversion
+				return CellIndex<dim, {false, idx_T.local, idx_T.md2macro, idx_T.noGhost}>(42);
+			}
+			else {
+				//TODO: dummy implementation, copy actual behaviour from IndexConversion
+				return index;
+			}	
+		}
+
+
 
 		template<unsigned int dim, IndexType idx_T>
 		CellIndex<dim, {false, idx_T.local, idx_T.md2macro, idx_T.noGhost}>
-		convertToVector(CellIndex<dim, idx_T> index);
-	}
-}
+		convertToVector(CellIndex<dim, idx_T> index) {
+			if constexpr(idx_T.vector) {
+				//TODO: dummy implementation, copy actual behaviour from IndexConversion
+				return index;
+			}
+			else {
+				//TODO: dummy implementation, copy actual behaviour from IndexConversion
+				return CellIndex<dim, {true, idx_T.local, idx_T.md2macro, idx_T.noGhost}>(tarch::la::Vector<dim, unsigned int>(42));
+			}
+		}
 
-using namespace coupling::indexing;
-
-template<unsigned int dim, IndexType idx_T>
-CellIndex<dim, {false, idx_T.local, idx_T.md2macro, idx_T.noGhost}>
-convertToScalar(CellIndex<dim, idx_T> index) {
-	if constexpr(idx_T.vector) {
-		//TODO: dummy implementation, copy actual behaviour from IndexConversion
-		return CellIndex<dim, {false, idx_T.local, idx_T.md2macro, idx_T.noGhost}>(42);
-	}
-	else {
-		//TODO: dummy implementation, copy actual behaviour from IndexConversion
-		return index;
-	}	
-}
-
-template<unsigned int dim, IndexType idx_T>
-CellIndex<dim, {true, idx_T.local, idx_T.md2macro, idx_T.noGhost}>
-convertToVector(CellIndex<dim, idx_T> index) {
-	if constexpr(idx_T.vector) {
-		//TODO: dummy implementation, copy actual behaviour from IndexConversion
-		return index;
-	}
-	else {
-		//TODO: dummy implementation, copy actual behaviour from IndexConversion
-		return CellIndex<dim, {true, idx_T.local, idx_T.md2macro, idx_T.noGhost}>(tarch::la::Vector<dim, unsigned int>(42));
 	}
 }
