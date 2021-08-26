04a95f5a41e0cfc5d49045691ca43d66b9cbd681
Felix Maurer <felix.maurer@informatik.uni-hamburg.de>
2021-08-26 17:51:35 +0200

    feat: decl md2macro CellIndex specialisations

--- coupling/indexing/CellIndexDecls.h
+++ coupling/indexing/CellIndexDecls.h
@@ -3,6 +3,19 @@
 namespace coupling {
 	namespace indexing {
 
+		/*
+		 * NOTE:
+		 * This currently uses hardcoded values for one specific KVS test scenario.
+		 * In addition to this, it is aimed to be tested sequentially, i.e. .local shall behave identical to !.local
+		 *
+		 * TODO:
+		 * Only declarations should be made here, lower/upper boundaries must be determined at runtime using IndexingService
+		 */
+
+		/*
+		 * !MD TO MACRO aka MAMICO INDEXING
+		 */
+
 		//BaseIndex
 		template class CellIndex<3, BaseIndexType>;
 		template<>
@@ -10,27 +23,58 @@ namespace coupling {
 		template<>
 		CellIndex<3, BaseIndexType> CellIndex<3, BaseIndexType>::upperBoundary {0};
 
-		//scalar, global, not m2m, noGL
+		//scalar, global, not md2macro, noGL
 		template class CellIndex<3>;
 		template<>
 		CellIndex<3, BaseIndexType> CellIndex<3>::lowerBoundary {0};
 		template<>
 		CellIndex<3, BaseIndexType> CellIndex<3>::upperBoundary {0};
 
-		//vector, local, not m2m, noGL
+		//vector, local, not md2macro, noGL
 		template class CellIndex<3, {.vector=true, .local=true}>;
 		template<>
 		CellIndex<3, BaseIndexType> CellIndex<3, {.vector=true, .local=true}>::lowerBoundary {0};
 		template<>
 		CellIndex<3, BaseIndexType> CellIndex<3, {.vector=true, .local=true}>::upperBoundary {0};
 
-		//scalar, local, not m2m, noGL
+		//scalar, local, not md2macro, noGL
 		template class CellIndex<3, {.local=true}>;
 		template<>
 		CellIndex<3, BaseIndexType> CellIndex<3, {.local=true}>::lowerBoundary {0};
 		template<>
 		CellIndex<3, BaseIndexType> CellIndex<3, {.local=true}>::upperBoundary {0};
 
+		/*
+		 * MD TO MACRO
+		 */
+
+		//vector, global, md2macro, noGL
+		template class CellIndex<3, {.vector=true, .md2macro=true}>;
+		template<>
+		CellIndex<3, BaseIndexType> CellIndex<3, {.vector=true, .md2macro=true}>::lowerBoundary {4};
+		template<>
+		CellIndex<3, BaseIndexType> CellIndex<3, {.vector=true, .md2macro=true}>::upperBoundary {9};
+
+		//scalar, global, md2macro, noGL
+		template class CellIndex<3, {.md2macro=true}>;
+		template<>
+		CellIndex<3, BaseIndexType> CellIndex<3, {.md2macro=true}>::lowerBoundary {4};
+		template<>
+		CellIndex<3, BaseIndexType> CellIndex<3, {.md2macro=true}>::upperBoundary {9};
+
+		//vector, local, md2macro, noGL
+		template class CellIndex<3, {.vector=true, .local=true, .md2macro=true}>;
+		template<>
+		CellIndex<3, BaseIndexType> CellIndex<3, {.vector=true, .local=true, .md2macro=true}>::lowerBoundary {4};
+		template<>
+		CellIndex<3, BaseIndexType> CellIndex<3, {.vector=true, .local=true, .md2macro=true}>::upperBoundary {9};
+
+		//scalar, local, md2macro, noGL
+		template class CellIndex<3, {.local=true, .md2macro=true}>;
+		template<>
+		CellIndex<3, BaseIndexType> CellIndex<3, {.local=true, .md2macro=true}>::lowerBoundary {4};
+		template<>
+		CellIndex<3, BaseIndexType> CellIndex<3, {.local=true, .md2macro=true}>::upperBoundary {9};
 
 	}
 }
