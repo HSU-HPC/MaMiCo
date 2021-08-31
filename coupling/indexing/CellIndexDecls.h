1085618301f5261ca100f01eae016170760d240a
Felix Maurer <felix.maurer@informatik.uni-hamburg.de>
2021-08-31 12:37:56 +0200

    fix: conversion bug in Operations.h, proper default init in Decls

--- coupling/indexing/CellIndexDecls.h
+++ coupling/indexing/CellIndexDecls.h
@@ -10,6 +10,9 @@ namespace coupling {
 		 *
 		 * TODO:
 		 * Only declarations should be made here, lower/upper boundaries must be determined at runtime using IndexingService
+		 *
+		 * TODO:
+		 * move all of this to Service
 		 */
 
 		/*
@@ -23,50 +26,48 @@ namespace coupling {
 		template<>
 		CellIndex<3, BaseIndexType> CellIndex<3, BaseIndexType>::upperBoundary {13};
 		template<>
-		tarch::la::Vector<3, unsigned int> CellIndex<3, BaseIndexType>::numberCellsInDomain { 0 };
+		tarch::la::Vector<3, unsigned int> CellIndex<3, BaseIndexType>::numberCellsInDomain {};
 		template<>
-		tarch::la::Vector<3, unsigned int> CellIndex<3, BaseIndexType>::divisionFactor { 0 };
+		tarch::la::Vector<3, unsigned int> CellIndex<3, BaseIndexType>::divisionFactor {};
 
-		//scalar, global, not md2macro, noGL
+		//scalar, global, not md2macro, not noGL
 		template class CellIndex<3>;
 		template<>
 		CellIndex<3, BaseIndexType> CellIndex<3>::lowerBoundary {0};
 		template<>
 		CellIndex<3, BaseIndexType> CellIndex<3>::upperBoundary {13};
 		template<>
-		tarch::la::Vector<3, unsigned int> CellIndex<3>::numberCellsInDomain { 0 };
+		tarch::la::Vector<3, unsigned int> CellIndex<3>::numberCellsInDomain {};
 		template<>
-		tarch::la::Vector<3, unsigned int> CellIndex<3>::divisionFactor { 0 };
+		tarch::la::Vector<3, unsigned int> CellIndex<3>::divisionFactor {};
 
-		//vector, local, not md2macro, noGL
+		//vector, local, not md2macro, not noGL
 		template class CellIndex<3, {.vector=true, .local=true}>;
 		template<>
 		CellIndex<3, BaseIndexType> CellIndex<3, {.vector=true, .local=true}>::lowerBoundary {0};
 		template<>
 		CellIndex<3, BaseIndexType> CellIndex<3, {.vector=true, .local=true}>::upperBoundary {13};
 		template<>
-		tarch::la::Vector<3, unsigned int> CellIndex<3, {.vector=true, .local=true}>::numberCellsInDomain { 0 };
+		tarch::la::Vector<3, unsigned int> CellIndex<3, {.vector=true, .local=true}>::numberCellsInDomain {};
 		template<>
-		tarch::la::Vector<3, unsigned int> CellIndex<3, {.vector=true, .local=true}>::divisionFactor { 0 };
+		tarch::la::Vector<3, unsigned int> CellIndex<3, {.vector=true, .local=true}>::divisionFactor {};
 
- 
-
-		//scalar, local, not md2macro, noGL
+		//scalar, local, not md2macro, not noGL
 		template class CellIndex<3, {.local=true}>;
 		template<>
 		CellIndex<3, BaseIndexType> CellIndex<3, {.local=true}>::lowerBoundary {0};
 		template<>
 		CellIndex<3, BaseIndexType> CellIndex<3, {.local=true}>::upperBoundary {13};
 		template<>
-		tarch::la::Vector<3, unsigned int> CellIndex<3, {.local=true}>::numberCellsInDomain { 0 };
+		tarch::la::Vector<3, unsigned int> CellIndex<3, {.local=true}>::numberCellsInDomain {};
 		template<>
-		tarch::la::Vector<3, unsigned int> CellIndex<3, {.local=true}>::divisionFactor { 0 };
+		tarch::la::Vector<3, unsigned int> CellIndex<3, {.local=true}>::divisionFactor {};
 
 		/*
 		 * MD TO MACRO
 		 */
 
-		//vector, global, md2macro, noGL
+		//vector, global, md2macro, not noGL
 		template class CellIndex<3, {.vector=true, .md2macro=true}>;
 		template<>
 		CellIndex<3, BaseIndexType> CellIndex<3, {.vector=true, .md2macro=true}>::lowerBoundary {4};
@@ -77,38 +78,38 @@ namespace coupling {
 		template<>
 		tarch::la::Vector<3, unsigned int> CellIndex<3, {.vector=true, .md2macro=true}>::divisionFactor {0};
 
-		//scalar, global, md2macro, noGL
+		//scalar, global, md2macro, not noGL
 		template class CellIndex<3, {.md2macro=true}>;
 		template<>
 		CellIndex<3, BaseIndexType> CellIndex<3, {.md2macro=true}>::lowerBoundary {4};
 		template<>
 		CellIndex<3, BaseIndexType> CellIndex<3, {.md2macro=true}>::upperBoundary {9};
 		template<>
-		tarch::la::Vector<3, unsigned int> CellIndex<3, {.md2macro=true}>::numberCellsInDomain { 0 };
+		tarch::la::Vector<3, unsigned int> CellIndex<3, {.md2macro=true}>::numberCellsInDomain {};
 		template<>
-		tarch::la::Vector<3, unsigned int> CellIndex<3, {.md2macro=true}>::divisionFactor { 0 };
+		tarch::la::Vector<3, unsigned int> CellIndex<3, {.md2macro=true}>::divisionFactor {};
 
-		//vector, local, md2macro, noGL
+		//vector, local, md2macro, not noGL
 		template class CellIndex<3, {.vector=true, .local=true, .md2macro=true}>;
 		template<>
 		CellIndex<3, BaseIndexType> CellIndex<3, {.vector=true, .local=true, .md2macro=true}>::lowerBoundary {4};
 		template<>
 		CellIndex<3, BaseIndexType> CellIndex<3, {.vector=true, .local=true, .md2macro=true}>::upperBoundary {9};
 		template<>
-		tarch::la::Vector<3, unsigned int> CellIndex<3, {.vector=true, .local=true, .md2macro=true}>::numberCellsInDomain { 0 };
+		tarch::la::Vector<3, unsigned int> CellIndex<3, {.vector=true, .local=true, .md2macro=true}>::numberCellsInDomain {};
 		template<>
-		tarch::la::Vector<3, unsigned int> CellIndex<3, {.vector=true, .local=true, .md2macro=true}>::divisionFactor { 0 };
+		tarch::la::Vector<3, unsigned int> CellIndex<3, {.vector=true, .local=true, .md2macro=true}>::divisionFactor {};
 
-		//scalar, local, md2macro, noGL
+		//scalar, local, md2macro, not noGL
 		template class CellIndex<3, {.local=true, .md2macro=true}>;
 		template<>
 		CellIndex<3, BaseIndexType> CellIndex<3, {.local=true, .md2macro=true}>::lowerBoundary {4};
 		template<>
 		CellIndex<3, BaseIndexType> CellIndex<3, {.local=true, .md2macro=true}>::upperBoundary {9};
 		template<>
-		tarch::la::Vector<3, unsigned int> CellIndex<3, {.local=true, .md2macro=true}>::numberCellsInDomain { 0 };
+		tarch::la::Vector<3, unsigned int> CellIndex<3, {.local=true, .md2macro=true}>::numberCellsInDomain {};
 		template<>
-		tarch::la::Vector<3, unsigned int> CellIndex<3, {.local=true, .md2macro=true}>::divisionFactor { 0 };
+		tarch::la::Vector<3, unsigned int> CellIndex<3, {.local=true, .md2macro=true}>::divisionFactor {};
 
 	}
 }
