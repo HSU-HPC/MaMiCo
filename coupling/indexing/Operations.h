e2c0a3ab256985bac81148a012e758346c8125aa
Felix Maurer <felix.maurer@informatik.uni-hamburg.de>
2021-08-31 12:37:56 +0200

    fix: conversion bug in Operations.h, proper default init in Decls

--- coupling/indexing/Operations.h
+++ coupling/indexing/Operations.h
@@ -17,10 +17,9 @@ namespace coupling {
 			if constexpr(idx_T.vector) {
 				//copied from deprecated coupling::IndexConversion::getCellIndex())
 				
-				unsigned int i = index.get()[dim-1];
+				unsigned int i { index.get()[dim-1] };
 				for (int d = dim-2; d >-1; d--){
-					//TODO: introduce operator[] for .vector=true CellIndex?
-					i = (CellIndex<dim, idx_T>::numberCellsInDomain[d]+2)*i + index.get()[d];
+					i = (CellIndex<dim, idx_T>::numberCellsInDomain[d])*i + index.get()[d];
 				}
 
 				return CellIndex<dim, {false, idx_T.local, idx_T.md2macro, idx_T.noGhost}> { i };
@@ -32,7 +31,6 @@ namespace coupling {
 		}
 
 
-		//TODO: fixme
 		template<unsigned int dim, IndexType idx_T>
 		CellIndex<dim, {true, idx_T.local, idx_T.md2macro, idx_T.noGhost}>
 		convertToVector(CellIndex<dim, idx_T> index) {
@@ -44,7 +42,7 @@ namespace coupling {
 				//copied from coupling::getVectorCellIndex()
 
 				tarch::la::Vector<dim,unsigned int> i { 0 };
-				unsigned int i_sca {index.get()};
+				unsigned int i_sca { index.get() };
 				for (int d = dim-1; d> 0; d--){
 					i[d] = i_sca / CellIndex<dim, idx_T>::divisionFactor[d];
 					i_sca -= i[d]*CellIndex<dim, idx_T>::divisionFactor[d];
