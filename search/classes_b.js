44646abba4543b12075f5e5a4c4b0be842d40ac9
jafariv <jafariv@users.noreply.github.com>
2021-10-18 14:41:16 +0000

    deploy: c1ce09477adebe47f34175c2460bdb7c8923b802

--- search/classes_b.js
+++ search/classes_b.js
@@ -26,6 +26,5 @@ var searchData=
   ['momentuminsertion_23',['MomentumInsertion',['../classcoupling_1_1MomentumInsertion.html',1,'coupling']]],
   ['momentuminsertionconfiguration_24',['MomentumInsertionConfiguration',['../classcoupling_1_1configurations_1_1MomentumInsertionConfiguration.html',1,'coupling::configurations']]],
   ['multimdcellservice_25',['MultiMDCellService',['../classcoupling_1_1services_1_1MultiMDCellService.html',1,'coupling::services']]],
-  ['multimdmediator_26',['MultiMDMediator',['../classcoupling_1_1MultiMDMediator.html',1,'coupling']]],
-  ['multimdservice_27',['MultiMDService',['../classtarch_1_1utils_1_1MultiMDService.html',1,'tarch::utils']]]
+  ['multimdservice_26',['MultiMDService',['../classtarch_1_1utils_1_1MultiMDService.html',1,'tarch::utils']]]
 ];
