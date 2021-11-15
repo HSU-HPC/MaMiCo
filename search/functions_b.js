e7364f08945b7705861ce735d40bf884adf4f9ff
jafariv <jafariv@users.noreply.github.com>
2021-11-15 16:23:27 +0000

    deploy: f8c60885a42169f930fc798722e2f6c97e3b61ae

--- search/functions_b.js
+++ search/functions_b.js
@@ -4,7 +4,10 @@ var searchData=
   ['macroscopiccellplotter_1',['MacroscopicCellPlotter',['../classcoupling_1_1MacroscopicCellPlotter.html#a15d064bc1c44cd2509755b90dd2df7d7',1,'coupling::MacroscopicCellPlotter']]],
   ['macroscopiccellserviceimpl_2',['MacroscopicCellServiceImpl',['../classcoupling_1_1services_1_1MacroscopicCellServiceImpl.html#a6a48f784afe734f308b2d79267867852',1,'coupling::services::MacroscopicCellServiceImpl']]],
   ['macroscopiccellwithlinkedcells_3',['MacroscopicCellWithLinkedCells',['../classcoupling_1_1datastructures_1_1MacroscopicCellWithLinkedCells.html#aa78716f6bec251b4f38be6fe2793ea07',1,'coupling::datastructures::MacroscopicCellWithLinkedCells']]],
-  ['moleculeextractor_4',['MoleculeExtractor',['../classcoupling_1_1cellmappings_1_1MoleculeExtractor.html#a842b774d1d8e92f235efcdb564e0ef95',1,'coupling::cellmappings::MoleculeExtractor']]],
-  ['momentumcontroller_5',['MomentumController',['../classcoupling_1_1MomentumController.html#a20a13847aebdc85c639e61c2820732e8',1,'coupling::MomentumController']]],
-  ['momentuminsertion_6',['MomentumInsertion',['../classcoupling_1_1MomentumInsertion.html#af10b00331787d64c1a0fdba158d8abec',1,'coupling::MomentumInsertion']]]
+  ['macroscopicsolverinterface_4',['MacroscopicSolverInterface',['../classcoupling_1_1interface_1_1MacroscopicSolverInterface.html#aabc9a3c07ba90a3e8cdb0c0adcade7ae',1,'coupling::interface::MacroscopicSolverInterface']]],
+  ['mamicointerfaceprovider_5',['MamicoInterfaceProvider',['../classcoupling_1_1interface_1_1MamicoInterfaceProvider.html#a62fe5d573e4c1ca160e1a6b7b484f0c6',1,'coupling::interface::MamicoInterfaceProvider']]],
+  ['moleculeextractor_6',['MoleculeExtractor',['../classcoupling_1_1cellmappings_1_1MoleculeExtractor.html#a842b774d1d8e92f235efcdb564e0ef95',1,'coupling::cellmappings::MoleculeExtractor']]],
+  ['moleculeiterator_7',['MoleculeIterator',['../classcoupling_1_1interface_1_1MoleculeIterator.html#a628874b05d58d9a1bb93a8b2a90de242',1,'coupling::interface::MoleculeIterator']]],
+  ['momentumcontroller_8',['MomentumController',['../classcoupling_1_1MomentumController.html#a20a13847aebdc85c639e61c2820732e8',1,'coupling::MomentumController']]],
+  ['momentuminsertion_9',['MomentumInsertion',['../classcoupling_1_1MomentumInsertion.html#af10b00331787d64c1a0fdba158d8abec',1,'coupling::MomentumInsertion']]]
 ];
