2e8e3559c5b52deaeb933cbcceb9e554ab05cac0
jafariv <jafariv@users.noreply.github.com>
2021-11-17 15:33:57 +0000

    deploy: 54f59cf6e653d4572b3879afdf2d61b4dfd05a31

--- search/namespaces_0.js
+++ search/namespaces_0.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['coupling_0',['coupling',['../namespacecoupling.html',1,'']]],
-  ['paralleltopology_1',['paralleltopology',['../namespacecoupling_1_1paralleltopology.html',1,'coupling']]],
-  ['solvers_2',['solvers',['../namespacecoupling_1_1solvers.html',1,'coupling']]],
-  ['transferstrategies_3',['transferstrategies',['../namespacecoupling_1_1transferstrategies.html',1,'coupling']]]
+  ['configurations_0',['configurations',['../namespacecoupling_1_1configurations.html',1,'coupling']]],
+  ['coupling_1',['coupling',['../namespacecoupling.html',1,'']]],
+  ['paralleltopology_2',['paralleltopology',['../namespacecoupling_1_1paralleltopology.html',1,'coupling']]],
+  ['solvers_3',['solvers',['../namespacecoupling_1_1solvers.html',1,'coupling']]],
+  ['transferstrategies_4',['transferstrategies',['../namespacecoupling_1_1transferstrategies.html',1,'coupling']]]
 ];
