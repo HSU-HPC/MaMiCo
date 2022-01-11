16358fe16b03407ddbba36ac938fdcadd1583454
Louis Viot <louis.viot@protonmail.com>
2022-01-11 15:39:44 +0100

    build file modification

--- coupling/tests/build_lammps/buildtest_lammps.sh
+++ coupling/tests/build_lammps/buildtest_lammps.sh
@@ -19,8 +19,8 @@ rm ${BUILD_PATH}/test_lammps;
 rm ${BUILD_PATH}/*.o;
 
 ${COMPILER} ${MAMICO_PATH}/tarch/tinyxml2/tinyxml2.cpp ${DEFINES} -c -o ${BUILD_PATH}/tinyxml2.o
-${COMPILER} ${MAMICO_PATH}/coupling/configurations/ParticleInsertionConfiguration.cpp ${DEFINES} -I${MPI_PATH_INCLUDE} -I${MAMICO_PATH} -I${LAMMPS_PATH} -c -o ${BUILD_PATH}/ParticleInsertionConfiguration.o
-${COMPILER} ${MAMICO_PATH}/tarch/utils/RandomNumberService.cpp ${DEFINES} -I${MPI_PATH_INCLUDE} -I${MAMICO_PATH} -I${LAMMPS_PATH} -c -o ${BUILD_PATH}/RandomNumberService.o
-${COMPILER} ${MAMICO_PATH}/coupling/tests/main_lammps.cpp ${DEFINES} -I${MPI_PATH_INCLUDE} -I${MAMICO_PATH} -I${LAMMPS_PATH} -c -o ${BUILD_PATH}/main_lammps.o
+${COMPILER} ${MAMICO_PATH}/coupling/configurations/ParticleInsertionConfiguration.cpp ${DEFINES} -I${MPI_INCLUDE_PATH} -I${MAMICO_PATH} -I${LAMMPS_PATH} -c -o ${BUILD_PATH}/ParticleInsertionConfiguration.o
+${COMPILER} ${MAMICO_PATH}/tarch/utils/RandomNumberService.cpp ${DEFINES} -I${MPI_INCLUDE_PATH} -I${MAMICO_PATH} -I${LAMMPS_PATH} -c -o ${BUILD_PATH}/RandomNumberService.o
+${COMPILER} ${MAMICO_PATH}/coupling/tests/main_lammps.cpp ${DEFINES} -I${MPI_INCLUDE_PATH} -I${MPI_INCLUDE_PATH} -I${LAMMPS_PATH} -c -o ${BUILD_PATH}/main_lammps.o
 ${COMPILER} -o ${BUILD_PATH}/test_lammps ${BUILD_PATH}/tinyxml2.o ${BUILD_PATH}/RandomNumberService.o ${BUILD_PATH}/ParticleInsertionConfiguration.o ${BUILD_PATH}/main_lammps.o -L${MPI_PATH_LIB} -l${LIB_MPI} -L${LAMMPS_PATH} -l${LIB_LAMMPS}
 
