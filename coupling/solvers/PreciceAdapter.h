55e16bcdb7e8e1e903c4660b00cd203cdb325dfb
Louis Viot <louis.viot@protonmail.com>
2022-05-19 16:28:13 +0200

    Adding connectivity in the mamico mesh given to precice

--- coupling/solvers/PreciceAdapter.h
+++ coupling/solvers/PreciceAdapter.h
@@ -48,7 +48,7 @@ public:
   void setInterface(const unsigned int overlap) { _overlap = overlap; }
 
   void setCouplingMesh(const tarch::la::Vector<3, double> mdDomainOffset, const tarch::la::Vector<3, double> macroscopicCellSize,
-    const unsigned int* const recvIndices, size_t recvIndicesSize) {
+    const unsigned int* const mamicoRecvdIndices, size_t mamicoRecvdIndicesSize) {
     _mdDomainOffset = mdDomainOffset;
     _macroscopicCellSize = macroscopicCellSize;
     int rank = 0;
@@ -66,15 +66,15 @@ public:
     }
     std::cout << "MaMiCo >> mamico-M2m-mesh size: " << _numberOfM2mCells << std::endl;
     _coordsM2mCells = new double[dim * _numberOfM2mCells];
-    unsigned int index = 0;
+    unsigned int vertexPreciceID = 0;
     for (CellIndex<dim, IndexTrait::vector> cellIndex : CellIndex<dim, IndexTrait::vector>()) {
       tarch::la::Vector<3, unsigned int> cellVectorIndex = static_cast<tarch::la::Vector<dim, unsigned int>>(cellIndex.get());
       if (sendMacroscopicQuantityToMDSolver(cellVectorIndex)) {
         for (unsigned int currentDim = 0; currentDim < dim; currentDim++) {
-          _coordsM2mCells[dim * index + currentDim] =
+          _coordsM2mCells[dim * vertexPreciceID + currentDim] =
               mdDomainOffset[currentDim] + cellVectorIndex[currentDim] * _dx - _dx + 0.5 * macroscopicCellSize[currentDim];
         }
-        index++;
+        vertexPreciceID++;
       }
     }
     _vertexM2mCellIDs = new int[_numberOfM2mCells];
@@ -82,57 +82,74 @@ public:
     _velocityM2mCells = new double[_numberOfM2mCells * dim];
 
     _numberOfm2MCells = 0;
-    for (size_t indexRecvIndices = 0; indexRecvIndices < recvIndicesSize; indexRecvIndices++) {
-      const unsigned int recvIndex = recvIndices[indexRecvIndices];
-      tarch::la::Vector<3, unsigned int> recvVectorIndex = static_cast<tarch::la::Vector<dim, unsigned int>>(coupling::indexing::convertToVector<dim>({recvIndex}));
-      if (receiveMacroscopicQuantityFromMDSolver(recvVectorIndex)) _numberOfm2MCells++;
+    for (size_t iMamicoRecvdIndices = 0; iMamicoRecvdIndices < mamicoRecvdIndicesSize; iMamicoRecvdIndices++) {
+      const unsigned int mamicoRecvdIndex = mamicoRecvdIndices[iMamicoRecvdIndices];
+      tarch::la::Vector<3, unsigned int> mamicoRecvdVIndex = static_cast<tarch::la::Vector<dim, unsigned int>>(coupling::indexing::convertToVector<dim>({mamicoRecvdIndex}));
+      if (receiveMacroscopicQuantityFromMDSolver(mamicoRecvdVIndex)) _numberOfm2MCells++;
     }
     std::cout << "MaMiCo >> mamico-m2M-mesh size: " << _numberOfm2MCells << std::endl;
 
     _coordsm2MCells = new double[dim * _numberOfm2MCells];
-    int cellIndex = 0;
-    for (size_t indexRecvIndices = 0; indexRecvIndices < recvIndicesSize; indexRecvIndices++) {
-      const unsigned int recvIndex = recvIndices[indexRecvIndices];
-      tarch::la::Vector<3, unsigned int> recvVectorIndex = static_cast<tarch::la::Vector<dim, unsigned int>>(coupling::indexing::convertToVector<dim>({recvIndex}));
-      if (receiveMacroscopicQuantityFromMDSolver(recvVectorIndex)) {
+    vertexPreciceID = 0;
+    for (size_t iMamicoRecvdIndices = 0; iMamicoRecvdIndices < mamicoRecvdIndicesSize; iMamicoRecvdIndices++) {
+      const unsigned int mamicoRecvdIndex = mamicoRecvdIndices[iMamicoRecvdIndices];
+      tarch::la::Vector<3, unsigned int> mamicoRecvdVIndex = static_cast<tarch::la::Vector<dim, unsigned int>>(coupling::indexing::convertToVector<dim>({mamicoRecvdIndex}));
+      if (receiveMacroscopicQuantityFromMDSolver(mamicoRecvdVIndex)) {
         for (unsigned int currentDim = 0; currentDim < dim; currentDim++) {
-          _coordsm2MCells[dim * cellIndex + currentDim] =
-              _mdDomainOffset[currentDim] + recvVectorIndex[currentDim] * _dx - _dx + 0.5 * _macroscopicCellSize[currentDim];
+          _coordsm2MCells[dim * vertexPreciceID + currentDim] =
+              _mdDomainOffset[currentDim] + mamicoRecvdVIndex[currentDim] * _dx - _dx + 0.5 * _macroscopicCellSize[currentDim];
         }
-        cellIndex++;
+        vertexPreciceID++;
       }
     }
     _vertexm2MCellIDs = new int[_numberOfm2MCells];
     _interface->setMeshVertices(_interface->getMeshID("mamico-m2M-mesh"), _numberOfm2MCells, _coordsm2MCells, _vertexm2MCellIDs);
-    /*for (size_t i = 0; i < _numberOfm2MCells; i++) {
-      std::cout << "vertex id:" << _vertexm2MCellIDs[i]
-                << ", coords:[" << _coordsm2MCells[dim* i]
-                << "," << _coordsm2MCells[dim* i+1]
-                << "," << _coordsm2MCells[dim* i +2]
-                << "]" << std::endl;
-    }*/
     _velocitym2MCells = new double[_numberOfm2MCells * dim];
 
-    cellIndex = 0;
-    tarch::la::Vector<3, int> directions[12] = {{1,0,-1}, {0,1,-1}, {-1,0,-1}, {0,-1,-1}, {1,0,0}, {0,1,0}, {-1,0,0}, {0,-1,0}, {1,0,1}, {0,1,1}, {-1,0,1}, {0,-1,1}};
-    int directionIndices[12];
-    for (size_t indexRecvIndices = 0; indexRecvIndices < recvIndicesSize; indexRecvIndices++) {
-      const unsigned int recvIndex = recvIndices[indexRecvIndices];
-      tarch::la::Vector<3, int> recvVectorIndex = coupling::indexing::convertToVector<dim>({recvIndex});
-      if (receiveMacroscopicQuantityFromMDSolver(static_cast<tarch::la::Vector<dim, unsigned int>>(recvVectorIndex))) {
-        for(size_t i=0; i < 12, i++) {
-          tarch::la::Vector<3, int> neighborVectorIndex = recvVectorIndex + directions[i];
-          if (receiveMacroscopicQuantityFromMDSolver(static_cast<tarch::la::Vector<dim, unsigned int>>(neighborVectorIndex))) {
-              int neighborIndex = coupling::indexing::convertToScalar<dim>(neighborVectorIndex);
-              int neighborIndexRecvIndices = 0;
-              while (neighborIndexRecvIndices < recvIndicesSize && recvIndices[neighborIndexRecvIndices] != neighborIndex) neighborIndexRecvIndices++;
-              directionIndices[i]=neighborIndexRecvIndices
-          }
+    vertexPreciceID = 0;
+    for (size_t iMamicoRecvdIndices = 0; iMamicoRecvdIndices < mamicoRecvdIndicesSize; iMamicoRecvdIndices++) {
+      const unsigned int mamicoRecvdIndex = mamicoRecvdIndices[iMamicoRecvdIndices];
+      tarch::la::Vector<3, int> mamicoRecvdVIndex = coupling::indexing::convertToVector<dim>({mamicoRecvdIndex});
+      if (receiveMacroscopicQuantityFromMDSolver(static_cast<tarch::la::Vector<dim, unsigned int>>(mamicoRecvdVIndex))) {
+        std::vector<int> triangleNodeIDs = getTriangleNodeIDs(vertexPreciceID, mamicoRecvdVIndex, mamicoRecvdIndices, mamicoRecvdIndicesSize);
+        for (size_t triangleIndex = 0; triangleIndex < triangleNodeIDs.size()/3; triangleIndex++) {
+          _interface->setMeshTriangleWithEdges(_interface->getMeshID("mamico-m2M-mesh"), triangleNodeIDs[triangleIndex*3], triangleNodeIDs[triangleIndex*3+1], triangleNodeIDs[triangleIndex*3+2]);
         }
-        cellIndex++;
+        vertexPreciceID++;
       }
     }
     _precice_dt = _interface->initialize();
+  }
+
+  std::vector<int> getTriangleNodeIDs(const int nodePreciceID, const tarch::la::Vector<3, int> nodeMamicoVIndex, const unsigned int* const mamicoRecvdIndices, size_t mamicoRecvdIndicesSize) {
+    std::vector<int> triangleNodePreciceIDs;
+    const int numberOfNeighbors = 6;
+    tarch::la::Vector<3, int> directionNeighbors[numberOfNeighbors] = {{0,0,-1},{1,0,0}, {0,1,0}, {-1,0,0}, {0,-1,0},{1,0,1}};
+    int neighborVertexIDSs[numberOfNeighbors];
+    for (size_t iDirectionNeighbors= 0; iDirectionNeighbors < numberOfNeighbors; iDirectionNeighbors++) {
+      tarch::la::Vector<3, int> neighborMamicoVIndex = nodeMamicoVIndex + directionNeighbors[iDirectionNeighbors];
+      if (receiveMacroscopicQuantityFromMDSolver(static_cast<tarch::la::Vector<dim, unsigned int>>(neighborMamicoVIndex))) {
+        using coupling::indexing::IndexTrait;
+        using coupling::indexing::CellIndex;
+        using coupling::indexing::convertToScalar;
+        int neighborMamicoIndex = convertToScalar<dim>(CellIndex<dim,IndexTrait::vector>(neighborMamicoVIndex));
+        int neighborPreciceID = 0;
+        while (neighborPreciceID < (int)mamicoRecvdIndicesSize && (int)mamicoRecvdIndices[neighborPreciceID] != neighborMamicoIndex) neighborPreciceID++;
+        neighborVertexIDSs[iDirectionNeighbors]=neighborPreciceID;
+      } else {
+        neighborVertexIDSs[iDirectionNeighbors]=-1;
+      }
+    }
+    for (size_t zNeighborIndex = 0; zNeighborIndex < 2; zNeighborIndex++) {
+      if (neighborVertexIDSs[zNeighborIndex] > 0) {
+        for (size_t xyNeighborIndex = 2; xyNeighborIndex < 6; xyNeighborIndex++) {
+          if (neighborVertexIDSs[xyNeighborIndex] > 0) {
+            triangleNodePreciceIDs.insert(triangleNodePreciceIDs.end(), {nodePreciceID, neighborVertexIDSs[zNeighborIndex], neighborVertexIDSs[xyNeighborIndex]});
+          }
+        }
+      }
+    }
+    return triangleNodePreciceIDs;
   }
 
   void setWallVelocity(const tarch::la::Vector<3, double> wallVelocity) override { _wallVelocity = wallVelocity; }
@@ -209,14 +226,14 @@ public:
   }
 
 
-  void setMDBoundaryValues(std::vector<coupling::datastructures::MacroscopicCell<3>*>& recvBuffer, const unsigned int* const recvIndices) {
+  void setMDBoundaryValues(std::vector<coupling::datastructures::MacroscopicCell<3>*>& recvBuffer, const unsigned int* const mamicoRecvIndices) {
     std::cout << "setMDBoundaryValues" << std::endl;
-    const size_t recvIndicesSize = recvBuffer.size();
+    const size_t mamicoRecvIndicesSize = recvBuffer.size();
     int cellIndex = 0;
-    for (size_t indexRecvIndices = 0; indexRecvIndices < recvIndicesSize; indexRecvIndices++) {
-      const unsigned int recvIndex = recvIndices[indexRecvIndices];
+    for (size_t indexRecvIndices = 0; indexRecvIndices < mamicoRecvIndicesSize; indexRecvIndices++) {
+      const unsigned int recvIndex = mamicoRecvIndices[indexRecvIndices];
       tarch::la::Vector<3, unsigned int> recvVectorIndex = static_cast<tarch::la::Vector<dim, unsigned int>>(coupling::indexing::convertToVector<dim>({recvIndex}));
-      if (sendMacroscopicQuantityToPreCICE(recvVectorIndex)) {
+      if (receiveMacroscopicQuantityFromMDSolver(recvVectorIndex)) {
         tarch::la::Vector<3, double> vel((1.0 / recvBuffer[indexRecvIndices]->getMacroscopicMass()) * recvBuffer[indexRecvIndices]->getMacroscopicMomentum());
         for (unsigned int currentDim = 0; currentDim < dim; currentDim++) _velocitym2MCells[dim * cellIndex + currentDim] = vel[currentDim];
         cellIndex++;
