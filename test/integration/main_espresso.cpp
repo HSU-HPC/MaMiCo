c0ad890f3e8d3a2cff84cd4e3fedd642d7a13565
Louis Viot <louis.viot@protonmail.com>
2022-12-13 16:57:17 +0100

    Moving files and splitting unit and integration tests

---
+++ test/integration/main_espresso.cpp
@@ -0,0 +1,45 @@
+// Copyright (C) 2015 Technische Universitaet Muenchen
+// This file is part of the Mamico project. For conditions of distribution
+// and use, please see the copyright notice in Mamico's main folder, or at
+// www5.in.tum.de/mamico
+#include "test/integration/TestEspresso.h"
+#include "test/integration/TestEspressoAddDeleteMolecule.h"
+#include "test/integration/TestEspressoForceEnergyCalculation.h"
+#include "test/integration/TestEspressoMDMolecule.h"
+#include "test/integration/TestEspressoMoleculeIterator.h"
+
+#include <mpi.h>
+
+void runTest(Test* test) {
+  if (test == NULL) {
+    std::cout << "ERROR executeTest: test==NULL!" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  test->run();
+  delete test;
+}
+
+int main(int argc, char* argv[]) {
+  int thisTest = atoi(argv[1]);
+  switch (thisTest) {
+  case 0:
+    runTest(new TestEspresso("TestEspresso", argc, argv));
+    break;
+  case 1:
+    runTest(new TestEspressoMDMolecule("TestEspressoMDMolecule", argc, argv));
+    break;
+  case 3:
+    runTest(new TestEspressoAddDeleteMolecule("TestEspressoAddDeleteMolecule", argc, argv));
+    break;
+  case 4:
+    runTest(new TestEspressoForceEnergyCalculation("TestEspressoForceEnergyCalculation", argc, argv));
+    break;
+  case 5:
+    runTest(new TestEspressoMoleculeIterator("TestEspressoMoleculeIterator", argc, argv));
+    break;
+  default:
+    std::cout << "Test number " << thisTest << " out of range..." << std::endl;
+  }
+
+  return 0;
+}
