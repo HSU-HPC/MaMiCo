5e59aca500c84cea15f3fc3930f634c4a1c2a236
amartyads <amartyads@users.noreply.github.com>
2023-12-30 18:25:05 +0000

    deploy: 74277f795141795fcffb82ac7761cebff218790f

--- search/classes_1.js
+++ search/classes_1.js
@@ -3,6 +3,5 @@ var searchData=
   ['block_0',['Block',['../structtinyxml2_1_1MemPoolT_1_1Block.html',1,'tinyxml2::MemPoolT']]],
   ['boundaryforceconfiguration_1',['BoundaryForceConfiguration',['../classcoupling_1_1configurations_1_1BoundaryForceConfiguration.html',1,'coupling::configurations']]],
   ['boundaryforcecontroller_2',['BoundaryForceController',['../classcoupling_1_1BoundaryForceController.html',1,'coupling']]],
-  ['buffercollective_3',['BufferCollective',['../structcoupling_1_1sendrecv_1_1SendReceiveBuffer_1_1BufferCollective.html',1,'coupling::sendrecv::SendReceiveBuffer']]],
-  ['bufferwithid_4',['BufferWithID',['../structcoupling_1_1sendrecv_1_1SendReceiveBuffer_1_1BufferWithID.html',1,'coupling::sendrecv::SendReceiveBuffer']]]
+  ['bufferwithid_3',['BufferWithID',['../structcoupling_1_1sendrecv_1_1SendReceiveBuffer_1_1BufferWithID.html',1,'coupling::sendrecv::SendReceiveBuffer']]]
 ];
