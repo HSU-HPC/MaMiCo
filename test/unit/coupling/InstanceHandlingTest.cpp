97bec93ccf5eca49cc239d87148b7b52751eb566
javariv <hsu@localhost.localdomain>
2024-02-19 14:11:30 +0100

    couette.xml dir

--- test/unit/coupling/InstanceHandlingTest.cpp
+++ test/unit/coupling/InstanceHandlingTest.cpp
@@ -26,7 +26,7 @@ public:
     
     void test() {
         
-        std::string filename("../test/unit/coupling/couette.xml");
+        std::string filename("../../test/unit/coupling/couette.xml");
         tarch::configuration::ParseConfiguration::parseConfiguration<simplemd::configurations::MolecularDynamicsConfiguration>(filename, "molecular-dynamics",_simpleMDConfig);
         
     if (!_simpleMDConfig.isValid()) {
