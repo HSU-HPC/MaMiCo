5b49f10aa32c7cd293e68a80b59ef92dd69913dd
javariv <hsu@localhost.localdomain>
2024-02-19 16:59:03 +0100

    InstanceHandlingTest.cpp modified

--- test/unit/coupling/InstanceHandlingTest.cpp
+++ test/unit/coupling/InstanceHandlingTest.cpp
@@ -52,9 +52,7 @@ public:
     }
     
 //    CPPUNIT_ASSERT(_instanceHandling->getSimpleMD().size() == _multiMDService->getTotalNumberOfMDSimulations());
-      CPPUNIT_ASSERT(_instanceHandling->getSimpleMD().size() == _multiMDService->getLocalNumberOfMDSimulations());
-//    CPPUNIT_ASSERT(_instanceHandling->getSimpleMD().size() == 2);
-    CPPUNIT_ASSERT(2 == _multiMDService->getTotalNumberOfMDSimulations());
+    CPPUNIT_ASSERT(_instanceHandling->getSimpleMD().size() == _multiMDService->getLocalNumberOfMDSimulations());
 //    CPPUNIT_ASSERT(_instanceHandling->getMDSolverInterface().size() != _multiMDService->getTotalNumberOfMDSimulations());
     CPPUNIT_ASSERT(_instanceHandling->getMDSolverInterface().size() != _multiMDService->getLocalNumberOfMDSimulations());
     _instanceHandling->setMDSolverInterface();
