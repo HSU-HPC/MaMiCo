201b734ea3423573603bcf91140cf47334fc3c43
Ruben Horn <>
2024-08-13 17:48:49 +0200

    config-generator: ls1 specific feedback

--- examples/config-generator/src/generators/solver_md.py
+++ examples/config-generator/src/generators/solver_md.py
@@ -24,8 +24,12 @@ def apply(partial_xml, get_config_value) -> None:
     key = __name__.split(".")[-1]
     solver = get_config_value(key)
     partial_xml.substitute("md-solver", solver)
-    wall_velocity = 0.5 if solver == "md" else 1.5
+    wall_velocity = 1.5 if solver == "ls1" else 0.5
     partial_xml.substitute("wall-velocity", wall_velocity)
+    thermostat_type = "all" if solver == "ls1" else "outerLayers"
+    partial_xml.substitute("thermostat-type", thermostat_type)
+    parallel_topology_type = "zyx" if solver == "ls1" else "xyz"
+    partial_xml.substitute("parallel-topology-type", parallel_topology_type)
     print("Substituted MD solver and wall velocity")
     if solver == "ls1":
         _create_ls1_config(get_config_value)
