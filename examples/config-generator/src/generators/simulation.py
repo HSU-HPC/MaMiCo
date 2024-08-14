1ff1c8dd17229f7b295b5b30b538640ceb86f53c
Ruben Horn <>
2024-08-14 10:11:46 +0200

    small bugfix

--- examples/config-generator/src/generators/simulation.py
+++ examples/config-generator/src/generators/simulation.py
@@ -7,7 +7,7 @@ def apply(partial_xml, get_config_value) -> None:
     domain_size = get_domain_size(get_config_value)
     if simulation_type == "test":
             partial_xml.substitute("coupling-cycles", 50)
-    elif simulation_type == "small":
+    elif simulation_type == "short":
             partial_xml.substitute("coupling-cycles", 250 * domain_size**2)
     elif simulation_type == "normal":
             partial_xml.substitute("coupling-cycles", 500 * domain_size**2)
