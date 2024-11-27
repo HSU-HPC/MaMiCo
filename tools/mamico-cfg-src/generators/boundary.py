358e2515eb0b9a116379e8dbff9277dbb700b779
Ruben Horn <>
2024-11-27 15:09:09 +0100

    Added XY-periodic boundary condition support in XML

--- tools/mamico-cfg-src/generators/boundary.py
+++ tools/mamico-cfg-src/generators/boundary.py
@@ -2,6 +2,8 @@ def apply(partial_xml, get_config_value) -> None:
     key = __name__.split(".")[-1]
     boundary_condition = get_config_value(key)
     boundary_condition_xml = None
+    boundary_condition_value = "periodic" if boundary_condition == "periodic" else "reflective"
+    boundary_condition_XY_value = "periodic" if "periodic" in boundary_condition else "reflective"
     if boundary_condition == "reflecting":
         boundary_condition_xml = """
 <particle-insertion type="usher" maximum-number-of-iterations="100" maximum-number-of-restarts="500" insert-every-timestep="10" tolerance="0.5" />
@@ -19,6 +21,7 @@ def apply(partial_xml, get_config_value) -> None:
 """.strip()
     else:
         raise ValueError(f'Invalid boundary condition "boundary_condition"')
-    partial_xml.substitute("boundary-condition", boundary_condition)
+    partial_xml.substitute("boundary-condition", boundary_condition_value)
+    partial_xml.substitute("boundary-condition-XY", boundary_condition_XY_value)
     partial_xml.substitute("boundary-condition-xml", boundary_condition_xml)
     print("Substituted boundary condition")
