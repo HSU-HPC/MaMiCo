d4b139b46e0953649577ac14e9f417f7f2a674ae
Ruben Horn <>
2025-01-30 13:56:22 +0100

    CI: comments in test cases file

--- test/integration/gh-act/get-test-cases-matrix.py
+++ test/integration/gh-act/get-test-cases-matrix.py
@@ -9,12 +9,19 @@ import json
 from pathlib import Path
 
 
+def clean_up_line(line):
+    line = line.split("#")[0]  # Remove comment
+    line = line.replace("\t", " ")
+    # line = line.replace("  ", " ")
+    line = line.strip()
+    return line
+
+
 def get_test_cases(filename):
     cells = []
     with open(filename, "r") as file:
-        lines = [
-            s.replace("\t", " ").replace("  ", " ").strip() for s in file.readlines()
-        ]
+        lines = [clean_up_line(s) for s in file.readlines()]
+        lines = [s for s in lines if len(s) > 0]  # Remove empty lines
         cells = [s.split() for s in lines]
     for row in cells[1:]:
         yield {k: v for k, v in zip(cells[0], row)}
