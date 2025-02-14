ff2dae104b5d29d7344696e8dd6486ee219847e9
Ruben Horn <>
2025-02-14 15:29:10 +0100

    Removed template argument from BoxCellContainer

--- coupling/datastructures/BoxCellContainer.h
+++ coupling/datastructures/BoxCellContainer.h
@@ -8,7 +8,7 @@
 
 namespace coupling {
 namespace datastructures {
-template <unsigned int dim> class BoxCellContainer;
+class BoxCellContainer;
 } // namespace datastructures
 } // namespace coupling
 
@@ -21,19 +21,25 @@ template <unsigned int dim> class BoxCellContainer;
  * using it. Due to this complete and contiguous nature of the datastructure, direct indexing is allowed, and the cell container is expected to start at
  * the relative location {0, 0, 0}. BoxCellContainers contain pointers to coupling cells, the cells are not owned by the container, hence
  * the calling function must delete the cells later.
- * @tparam dim Number of dimensions; it can be 1, 2 or 3
  */
-template <unsigned int dim> class coupling::datastructures::BoxCellContainer {
+class coupling::datastructures::BoxCellContainer {
 
 public:
   BoxCellContainer(I01 lowerBound, I01 upperBound) : BoxCellContainer({}, lowerBound, upperBound) {}
   
-  BoxCellContainer(I01 lowerBound, tarch::la::Vector<dim, unsigned int> shape) : BoxCellContainer({}, lowerBound, shape) {}
+  BoxCellContainer(I01 lowerBound, tarch::la::Vector<3, int> shape) : BoxCellContainer({}, lowerBound, shape) {}
 
-  BoxCellContainer(std::vector<coupling::datastructures::CouplingCell<dim>*> couplingCells, I01 lowerBound, I01 upperBound)
+  BoxCellContainer(std::vector<coupling::datastructures::CouplingCell<3>*> couplingCells, I01 lowerBound, I01 upperBound)
     : BoxCellContainer(couplingCells, lowerBound, upperBound.get() - lowerBound.get()) {}
 
-  BoxCellContainer(std::vector<coupling::datastructures::CouplingCell<dim>*> couplingCells, I01 lowerBound, tarch::la::Vector<dim, unsigned int> shape) {
+  BoxCellContainer(std::vector<coupling::datastructures::CouplingCell<3>*> couplingCells, I01 lowerBound, tarch::la::Vector<3, int> shape) {
+#if (COUPLING_MD_ERROR == COUPLING_MD_YES)
+    for (int i = 0; i < 3; i++)
+    if (shape[i] < 0) {
+        std::cout << "BoxCellContainer shape must not be negative, but dimension " << i << " was " << shape[i] << std::endl;
+        std::exit(EXIT_FAILURE);       
+    }
+#endif
     _lowerBound = lowerBound;
     _shape = shape;
     _couplingCells.reserve(linearNumberCellsInDomain());
@@ -46,11 +52,16 @@ public:
    * @param index index An index of type tarch::la::Vector
    * @return the pointer to the requested cell
    */
-  coupling::datastructures::CouplingCell<dim>* operator[](tarch::la::Vector<dim, unsigned int> index) const {
+  coupling::datastructures::CouplingCell<3>* operator[](tarch::la::Vector<3, int> index) const {
 #if (COUPLING_MD_ERROR == COUPLING_MD_YES)
-    // TODO implement bounds checking!
+    for (int i = 0; i < 3; i++)
+    if (index[i] < 0 || index[i] >= numberCellsInDomain()[i]) {
+      std::cout << "BoxCellContainer accessed out of bounds in dimension " << i;
+      std::cout << ": " << index[i] << " not in 0-" << numberCellsInDomain()[i] << std::endl;
+      std::exit(EXIT_FAILURE);
+    }
     if (_couplingCells.size() < linearNumberCellsInDomain()) {
-      std::cout << "BoxCellContainer<" << dim << "> accessed but not full " << std::endl;
+      std::cout << "BoxCellContainer accessed but not full " << std::endl;
       std::exit(EXIT_FAILURE);
     }
 #endif
@@ -60,10 +71,10 @@ public:
   /** Adds a new coupling cell to the datastructure at the next index (will only work if the data structure is not yet full)
    * @param cell a pointer to the cell to be inserted
    */
-  void operator<<(coupling::datastructures::CouplingCell<dim>* couplingCell) {
+  void operator<<(coupling::datastructures::CouplingCell<3>* couplingCell) {
 #if (COUPLING_MD_ERROR == COUPLING_MD_YES)
     if (_couplingCells.size() >= linearNumberCellsInDomain()) {
-      std::cout << "BoxCellContainer<" << "," << dim << "> can only hold " << linearNumberCellsInDomain() << " coupling cells!"
+      std::cout << "BoxCellContainer can only hold " << linearNumberCellsInDomain() << " coupling cells!"
                 << std::endl;
       std::exit(EXIT_FAILURE);
     }
@@ -74,80 +85,83 @@ public:
   /**
    * Returns size of the underlying container.
    *
-   * This size is notably not the number of cells in the index domain. However, in practice the two numbers should be identical, as accessing contents of an
+   * This size is notably not the number of cells in the box domain. However, in practice the two numbers should be identical, as accessing contents of an
    * incomplete cotnainer is not allowed.
    * @return the number of cells stored currently
    */
   unsigned int size() const { return _couplingCells.size(); }
 
-  tarch::la::Vector<dim, unsigned int> numberCellsInDomain() const {
+  /**
+   * Returns shape of the underlying box domain.
+   *
+   * This size is notably not the number of cells currently held by the datastructure. However, in practice the two numbers should be identical, as accessing contents of an
+   * incomplete cotnainer is not allowed.
+   * @return the number of cells in the box domain along each dimension (x, y, z)
+   */
+  tarch::la::Vector<3, int> numberCellsInDomain() const {
     return _shape;
   }
 
+  /**
+   * Returns size of the underlying box domain.
+   *
+   * This size is notably not the number of cells currently held by the datastructure. However, in practice the two numbers should be identical, as accessing contents of an
+   * incomplete cotnainer is not allowed.
+   * @return the number of cells in the box domain
+   */
   unsigned int linearNumberCellsInDomain() const {
     int count = 1;
-    for (int i = 0; i < dim; i++)
+    for (int i = 0; i < 3; i++)
         count *= _shape[i];
     return count;
   }
 
+  // FIXME: Iterator does not do bounds check yet -> using wrong indices
   /**
    * @brief Provides iterator functionality (increment, access as <*cell, index> pair, equality)
    */
-
-// TODO Implement iterator (FIXME)
-//  class Iterator {
-//  public:
-//    using CouplingCellIterator = typename std::vector<coupling::datastructures::CouplingCell<dim>*>::const_iterator;
-//
-//    Iterator(CouplingCellIterator itCouplingCells, typename I01::IndexIterator itIdx) : _itCouplingCells(itCouplingCells), _itIdx(itIdx) {}
-//
-//    /**
-//     * Iterator access, returning the data at the current iterator location
-//     *
-//     * @return a std::pair with the cell pointer and the index of the data that the iterator points to
-//     */
-//    const std::pair<coupling::datastructures::CouplingCell<dim>*, I01> operator*() const { return std::make_pair(*_itCouplingCells, *_itIdx); }
-//
-//    Iterator& operator++() {
-//      ++_itCouplingCells;
-//      ++_itIdx;
-//      return *this;
-//    }
-//
-//    Iterator operator++(int) {
-//      Iterator tmp = *this;
-//      ++(*this);
-//      return tmp;
-//    }
-//
-//    friend bool operator==(const Iterator& a, const Iterator& b) { return a._itCouplingCells == b._itCouplingCells; }
-//
-//    friend bool operator!=(const Iterator& a, const Iterator& b) { return !(a == b); }
-//
-//  private:
-//    /**Iterator to underlying cell* vector */
-//    CouplingCellIterator _itCouplingCells;
-//
-//    /**Iterator over the index subdomain
-//     *
-//     * As the container spans the whole index subdomain, the iterators already available for this index domain can be reused, and incremented in lockstep with
-//     * the internal iterator for the vector
-//     */
-//    typename CellIndexT::IndexIterator _itIdx;
-//  };
-//
-//  /** Provides pointer to beginning of iterator of this container */
-//  Iterator begin() const { return Iterator(_couplingCells.begin(), _lowerBound); }
-//
-//  /** Provides pointer to end of iterator of this container */
-//  Iterator end() const { return Iterator(_couplingCells.end(), _upperBound()); }
+  class Iterator {
+  public:
+    using CouplingCellIterator = typename std::vector<coupling::datastructures::CouplingCell<3>*>::const_iterator;
+    Iterator(CouplingCellIterator itCouplingCells, typename I01::IndexIterator itIdx) : _itCouplingCells(itCouplingCells), _itIdx(itIdx) {}
+    /**
+     * Iterator access, returning the data at the current iterator location
+     *
+     * @return a std::pair with the cell pointer and the index of the data that the iterator points to
+     */
+    const std::pair<coupling::datastructures::CouplingCell<3>*, I01> operator*() const { return std::make_pair(*_itCouplingCells, *_itIdx); }
+    Iterator& operator++() {
+      ++_itCouplingCells;
+      ++_itIdx;
+      return *this;
+    }
+    Iterator operator++(int) {
+      Iterator tmp = *this;
+      ++(*this);
+      return tmp;
+    }
+    friend bool operator==(const Iterator& a, const Iterator& b) { return a._itCouplingCells == b._itCouplingCells; }
+    friend bool operator!=(const Iterator& a, const Iterator& b) { return !(a == b); }
+  private:
+    /**Iterator to underlying cell* vector */
+    CouplingCellIterator _itCouplingCells;
+    /**Iterator over the index subdomain
+     *
+     * As the container spans the whole index subdomain, the iterators already available for this index domain can be reused, and incremented in lockstep with
+     * the internal iterator for the vector
+     */
+    typename I01::IndexIterator _itIdx;
+  };
+  /** Provides pointer to beginning of iterator of this container */
+  Iterator begin() const { return Iterator(_couplingCells.begin(), _lowerBound); }
+  /** Provides pointer to end of iterator of this container */
+  Iterator end() const { return Iterator(_couplingCells.end(), _upperBound()); }
 
 protected:
   /**
    * Holds pointers to all coupling cells.
    */
-  std::vector<coupling::datastructures::CouplingCell<dim>*> _couplingCells;
+  std::vector<coupling::datastructures::CouplingCell<3>*> _couplingCells;
   /**
    * Defines the lower corner of the box
    */
@@ -155,11 +169,11 @@ protected:
   /**
    * Defines the extent of the box along all dimensions
    */
-  tarch::la::Vector<dim, unsigned int> _shape;
+  tarch::la::Vector<3, int> _shape;
   /**
    * Defines the upper corner of the box
    */
-  I01 _upperBound() {
+  I01 _upperBound() const {
     return I01(_lowerBound + _shape);
   }
   
---
+++ test/unit/coupling/datastructures/BoxCellContainerTest.cpp
@@ -0,0 +1,85 @@
+#include "coupling/datastructures/BoxCellContainer.h"
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <tuple>
+#include <vector>
+
+using namespace coupling::indexing;
+
+class BoxCellContainerTest : public CppUnit::TestFixture {
+
+  CPPUNIT_TEST_SUITE(BoxCellContainerTest);
+  CPPUNIT_TEST(testIteration);
+  CPPUNIT_TEST_SUITE_END();
+
+public:
+  void setUp() {
+    using namespace coupling::datastructures;
+    _rank = 0;
+    _size = 1;
+#if (COUPLING_MD_PARALLEL == COUPLING_MD_YES)
+    MPI_Comm_size(MPI_COMM_WORLD, &_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &_rank);
+#endif
+    tarch::la::Vector<3, unsigned int> numberProcesses{1, 1, 1};
+    if (_size == 4)
+      numberProcesses = {2, 2, 1};
+    IndexingService<3>::getInstance().initWithCells(8, numberProcesses, coupling::paralleltopology::XYZ, 3, (unsigned int)_rank);
+    int i = 0;
+    _couplingCells_fullcase.reserve(I01::linearNumberCellsInDomain);
+    _idxs_fullcase.reserve(I01::linearNumberCellsInDomain);
+    _couplingCells_local.reserve(I03::linearNumberCellsInDomain);
+    _idxs_local.reserve(I03::linearNumberCellsInDomain);
+    for (auto idx : I01()) {
+      auto tmp = new CouplingCell<3>;
+      tmp->setMacroscopicMass(i++);
+      _couplingCells_fullcase.push_back(tmp);
+      _idxs_fullcase.push_back(idx);
+    }
+    for (auto idx : I03()) {
+      _couplingCells_local.push_back(_couplingCells_fullcase[I00{idx}.get()]);
+      _idxs_local.push_back(idx);
+    }
+  }
+
+  void tearDown() {
+    IndexingService<3>::getInstance().finalize();
+    for (auto cell : _couplingCells_fullcase) {
+      if (cell != nullptr)
+        delete cell;
+    }
+    _couplingCells_fullcase.clear();
+  }
+
+  void testIteration() {
+    using namespace coupling::datastructures;
+    BoxCellContainer container(*I01::begin(), *I01::end());
+    int i = 0;
+    for (auto idx : I01()) {
+      (void)idx; // Avoid unused variable error
+      container << _couplingCells_fullcase[i++];
+    }
+    I01 idx;
+    CouplingCell<3>* couplingCell;
+    i = 0;
+    for (auto pair : container) {
+      std::tie(couplingCell, idx) = pair;
+      CPPUNIT_ASSERT_DOUBLES_EQUAL(couplingCell->getMacroscopicMass(), _couplingCells_fullcase[i]->getMacroscopicMass(), 1e-16);
+      CPPUNIT_ASSERT_EQUAL(idx, _idxs_fullcase[i]);
+      i++;
+    }
+
+    // TODO write more tests
+    // TODO modify setUp and tearDown to use other position/size (not indexing domains)
+
+  }
+
+private:
+  int _size, _rank;
+  std::vector<coupling::datastructures::CouplingCell<3>*> _couplingCells_fullcase;
+  std::vector<I01> _idxs_fullcase;
+  std::vector<coupling::datastructures::CouplingCell<3>*> _couplingCells_local;
+  std::vector<I03> _idxs_local;
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(BoxCellContainerTest);
