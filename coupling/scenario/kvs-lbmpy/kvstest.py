126725c8356c10ed285daf3b44d5c7b9e004889f
Piet Jarmatz <jarmatz@hsu-hh.de>
2025-03-22 16:05:05 +0100

    kvstest adios2 bugfixes

--- coupling/scenario/kvs-lbmpy/kvstest.py
+++ coupling/scenario/kvs-lbmpy/kvstest.py
@@ -306,7 +306,7 @@ class KVSTest():
                     self.adiosfile.write_attribute('timefactor', "inf")
                 else:
                     timefactor = self.adios2 * self.dt/(self.simpleMDConfig.getADIOS2Configuration().getWriteEveryTimestep() * self.simpleMDConfig.getSimulationConfiguration().getDt())
-                    print("timefactor:", timefactor)
+                    log.info("timefactor:", timefactor)
                     self.adiosfile.write_attribute('timefactor', str(timefactor))
     
         if self.rank==0:
@@ -361,19 +361,19 @@ class KVSTest():
             if self.adios2 > 0 and (cycle+1)%self.adios2 == 0:
                to_write = np.ascontiguousarray(self.macroscopicSolver.scen.velocity[:,:,:,:].data, dtype=np.float32)
                log.info("writing to adios2 " + str(type(to_write)) + " with shape " + str(to_write.shape) + " and dtype " + str(to_write.dtype))
-               shape = np.array(self.macroscopicSolver.scen.velocity[:,:,:,0].data.shape)*self.mamicoConfig.getMacroscopicCellConfiguration().getMacroscopicCellSize()
+               shape = np.array(self.macroscopicSolver.scen.velocity[:,:,:,0].data.shape)*self.mamicoConfig.getCouplingCellConfiguration().getCouplingCellSize()
                offset = [0.,0.,0.]
-               print(shape)
-               print(mdpos)
-               print(self.simpleMDConfig.getDomainConfiguration().getGlobalDomainSize())
+               log.debug(str(shape))
+               log.debug(str(mdpos))
+               log.debug(str(self.simpleMDConfig.getDomainConfiguration().getGlobalDomainSize()))
                offset[0] = -((mdpos[0]/to_write.shape[0]* shape[0])-( 0.5 * self.simpleMDConfig.getDomainConfiguration().getGlobalDomainSize()[0]))
                offset[1] = -((mdpos[1]/to_write.shape[1]* shape[1])-( 0.5 * self.simpleMDConfig.getDomainConfiguration().getGlobalDomainSize()[1]))
                offset[2] = -((mdpos[2]/to_write.shape[2]* shape[2])-( 0.5 * self.simpleMDConfig.getDomainConfiguration().getGlobalDomainSize()[2]))
-               print(offset)
+               log.debug(str((offset)))
                gb_to_write = np.array([offset[0], offset[1], offset[2], offset[0] + shape[0], offset[1] + shape[1], offset[2] + shape[2]])
-               print(gb_to_write)
-               self.adiosfile.write("global_box", gb_to_write, gb_to_write.shape, np.zeros_like(gb_to_write.shape), gb_to_write.shape)
-               self.adiosfile.write("velocity", to_write, to_write.shape, np.zeros_like(to_write.shape), to_write.shape)
+               log.debug(str((gb_to_write)))
+               self.adiosfile.write("global_box", gb_to_write, gb_to_write.shape, np.zeros_like(gb_to_write.shape).tolist(), gb_to_write.shape)
+               self.adiosfile.write("velocity", to_write, to_write.shape, np.zeros_like(to_write.shape).tolist(), to_write.shape)
                self.adiosfile.end_step()
 
         if self.cfg.getboolean("coupling", "send-from-macro-to-md"):
