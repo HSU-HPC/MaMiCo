7fb5c98503c5816f9be117234946f95cb8c1cc15
Piet Jarmatz <jarmatz@hsu-hh.de>
2025-07-22 15:58:18 +0200

    clang-format

--- coupling/solvers/LBCouetteSolverState.cpp
+++ coupling/solvers/LBCouetteSolverState.cpp
@@ -7,43 +7,43 @@
 using State = coupling::interface::PintableMacroSolverState;
 
 std::unique_ptr<State> coupling::solvers::LBCouetteSolverState::operator+(const State& rhs) {
-    const LBCouetteSolverState* other = dynamic_cast<const LBCouetteSolverState*>(&rhs);
+  const LBCouetteSolverState* other = dynamic_cast<const LBCouetteSolverState*>(&rhs);
 
-    #if (COUPLING_MD_ERROR == COUPLING_MD_YES)
-    if(other == nullptr){
-      std::cout << "ERROR LBCouetteSolverState operator+ type mismatch" << std::endl;
-      exit(EXIT_FAILURE);
-    }
-    if(other->_pdf.size() != _pdf.size()){
-      std::cout << "ERROR LBCouetteSolverState operator+ size mismatch" << std::endl;
-      exit(EXIT_FAILURE);
-    }
-    #endif
+#if (COUPLING_MD_ERROR == COUPLING_MD_YES)
+  if (other == nullptr) {
+    std::cout << "ERROR LBCouetteSolverState operator+ type mismatch" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  if (other->_pdf.size() != _pdf.size()) {
+    std::cout << "ERROR LBCouetteSolverState operator+ size mismatch" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+#endif
 
-    std::unique_ptr<LBCouetteSolverState> res = std::make_unique<LBCouetteSolverState>(*this);
-    for(std::vector<double>::size_type i=0; i<_pdf.size(); ++i)
-      res->_pdf[i] += other->_pdf[i];
+  std::unique_ptr<LBCouetteSolverState> res = std::make_unique<LBCouetteSolverState>(*this);
+  for (std::vector<double>::size_type i = 0; i < _pdf.size(); ++i)
+    res->_pdf[i] += other->_pdf[i];
 
-    return res;
+  return res;
 }
 
 std::unique_ptr<State> coupling::solvers::LBCouetteSolverState::operator-(const State& rhs) {
-	const LBCouetteSolverState* other = dynamic_cast<const LBCouetteSolverState*>(&rhs);
+  const LBCouetteSolverState* other = dynamic_cast<const LBCouetteSolverState*>(&rhs);
 
-	#if (COUPLING_MD_ERROR == COUPLING_MD_YES)
-	if(other == nullptr){
-	  std::cout << "ERROR LBCouetteSolverState operator- type mismatch" << std::endl;
-	  exit(EXIT_FAILURE);
-	}
-	if(other->_pdf.size() != _pdf.size()){
-	  std::cout << "ERROR LBCouetteSolverState operator- size mismatch" << std::endl;
-	  exit(EXIT_FAILURE);
-	}
-	#endif
+#if (COUPLING_MD_ERROR == COUPLING_MD_YES)
+  if (other == nullptr) {
+    std::cout << "ERROR LBCouetteSolverState operator- type mismatch" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  if (other->_pdf.size() != _pdf.size()) {
+    std::cout << "ERROR LBCouetteSolverState operator- size mismatch" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+#endif
 
-	std::unique_ptr<LBCouetteSolverState> res = std::make_unique<LBCouetteSolverState>(*this);
-	for(std::vector<double>::size_type i=0; i<_pdf.size(); ++i)
-	  res->_pdf[i] -= other->_pdf[i];
+  std::unique_ptr<LBCouetteSolverState> res = std::make_unique<LBCouetteSolverState>(*this);
+  for (std::vector<double>::size_type i = 0; i < _pdf.size(); ++i)
+    res->_pdf[i] -= other->_pdf[i];
 
-	return res;
+  return res;
 }
