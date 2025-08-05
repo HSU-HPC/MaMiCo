1c7e8b7512117c3a1a626227869a0d7d3b37bd8a
amartyads <amartyads@gmail.com>
2025-08-05 17:35:51 +0200

    separation of moleculecontainer into h and cpp, fixed sort

---
+++ simplemd/MoleculeContainer.cpp
@@ -0,0 +1,200 @@
+#include "simplemd/MoleculeContainer.h"
+
+simplemd::MoleculeContainer::MoleculeContainer(simplemd::services::ParallelTopologyService parallelTopologyService, int cellCapacity)
+    : _numCells(parallelTopologyService.getLocalNumberOfCells(true)), _cellCapacity(cellCapacity),
+      _moleculeData("moleculeData", parallelTopologyService.getLocalNumberOfCellsLinear(true), cellCapacity),
+      linkedCellNumMolecules("linkedCellNumMolecules", parallelTopologyService.getLocalNumberOfCellsLinear(true)) {
+  tarch::la::Vector<MD_DIM, unsigned int> bufferGlobal(0);
+  tarch::la::Vector<MD_DIM, unsigned int> bufferLocal(0);
+  _domainOffset = parallelTopologyService.getGlobalDomainOffset();
+  _meshWidth = parallelTopologyService.getMeshWidth();
+  bufferGlobal = parallelTopologyService.getGlobalIndexOfFirstCell();
+  bufferLocal = tarch::la::Vector<MD_DIM, unsigned int>(1);
+  for (unsigned int d = 0; d < MD_DIM; d++) {
+    _globalIndexOfFirstCell[d] = (int)bufferGlobal[d];
+    _localIndexOfFirstCell[d] = (int)bufferLocal[d];
+  }
+}
+
+void simplemd::MoleculeContainer::insert(int cellIdx, simplemd::Molecule& molecule) {
+  _moleculeData(cellIdx, linkedCellNumMolecules(cellIdx)) = molecule;
+  linkedCellNumMolecules(cellIdx) += 1;
+}
+
+void simplemd::MoleculeContainer::insert(simplemd::Molecule& molecule) { insert(positionToCellIndex(molecule.getPosition()), molecule); }
+
+void simplemd::MoleculeContainer::remove(int cellIdx, int moleculeIdx) {
+  _moleculeData(cellIdx, moleculeIdx) = _moleculeData(cellIdx, linkedCellNumMolecules(cellIdx) - 1);
+  linkedCellNumMolecules(cellIdx) -= 1;
+}
+
+void simplemd::MoleculeContainer::clearLinkedCell(int cellIdx) { linkedCellNumMolecules(cellIdx) = 0; }
+
+void simplemd::MoleculeContainer::sort(int cellIdx) { // set all outgoing molecules
+  for (size_t i = 0; i < linkedCellNumMolecules(cellIdx); i++) {
+    int curMolIdx = positionToCellIndex(_moleculeData(cellIdx, i).getPosition());
+    if (curMolIdx != cellIdx) { // if molecule does not belong to current cell anymore
+      // write data to target end
+      _moleculeData(curMolIdx, linkedCellNumMolecules(curMolIdx)) = _moleculeData(cellIdx, i);
+      // increment target end
+      linkedCellNumMolecules(curMolIdx)++;
+      // delete molecule at own position
+      remove(cellIdx, i);
+      // decrement iterator as the molecule at position i is now new
+      i--;
+    }
+  }
+}
+
+void simplemd::MoleculeContainer::sort() {
+  // sort all inner cells, exclude all ghost cells
+  // find red-black cells
+
+// iterate over the domain in a red-black manner
+#if (MD_DIM > 2)
+  for (unsigned int z = 0; z < 2; z++) {
+#endif
+#if (MD_DIM > 1)
+    for (unsigned int y = 0; y < 2; y++) {
+#endif
+      for (unsigned int x = 0; x < 2; x++) {
+        // determine range/ length of blocks for red-black traversal.
+        // For odd block sizes, we need to do some more work in the
+        // x/y/z==0-traversals. The second x/y/z==1-traversals are reduced by
+        // the normal integer-rounding in this case.
+        const tarch::la::Vector<MD_DIM, unsigned int> lengthVector((_numCells[0] + ((_numCells[0]) % 2) * (x == 0)) / 2
+#if (MD_DIM > 1)
+                                                                   ,
+                                                                   (_numCells[1] + ((_numCells[1]) % 2) * (y == 0)) / 2
+#endif
+#if (MD_DIM > 2)
+                                                                   ,
+                                                                   (_numCells[2] + ((_numCells[2]) % 2) * (z == 0)) / 2
+#endif
+        );
+        const int length = lengthVector[0]
+#if (MD_DIM > 1)
+                           * lengthVector[1]
+#endif
+#if (MD_DIM > 2)
+                           * lengthVector[2]
+#endif
+            ;
+
+        // parallelise loop for all cells that are to be traversed in this way
+        Kokkos::parallel_for(
+            length, KOKKOS_LAMBDA(const unsigned int j) {
+              // compute index of the current cell
+              unsigned int index = 0;
+#if (MD_DIM > 1)
+              int helpIndex1 = j;
+              int helpIndex2 = 0;
+#endif
+
+#if (MD_DIM > 2)
+              // determine plane within traversed block
+              helpIndex2 = helpIndex1 / (lengthVector[0] * lengthVector[1]);
+              // save rest of index in helpIndex1
+              helpIndex1 = helpIndex1 - helpIndex2 * (lengthVector[0] * lengthVector[1]);
+              // compute contribution to index (the starting 1 is the z coordinate of the first cell)
+              index += (2 * helpIndex2 + z) * _numCells[0] * _numCells[1];
+#endif
+#if (MD_DIM > 1)
+              // determine plane within traversed block
+              helpIndex2 = helpIndex1 / lengthVector[0];
+              // save rest of index in helpIndex1
+              helpIndex1 = helpIndex1 - helpIndex2 * lengthVector[0];
+              // compute contribution to index
+              index += (2 * helpIndex2 + y) * _numCells[0];
+              // compute contribution for last dimension
+              index += (2 * helpIndex1 + x);
+#else
+        index = 2 * j + x;
+#endif
+#if (MD_DEBUG == MD_YES)
+              std::cout << "Handle cell " << index << std::endl;
+#endif
+              auto linkedCellLocal(linkedCellNumMolecules);
+              auto moleculeDataLocal(_moleculeData);
+              for (size_t i = 0; i < linkedCellLocal(index); i++) {
+                int curMolIdx = positionToCellIndex(moleculeDataLocal(index, i).getPosition());
+                if (curMolIdx != index) { // if molecule does not belong to current cell anymore
+                  // write data to target end
+                  moleculeDataLocal(curMolIdx, linkedCellLocal(curMolIdx)) = moleculeDataLocal(index, i);
+                  // increment target end
+                  linkedCellLocal(curMolIdx)++;
+                  // delete molecule at own position
+                  moleculeDataLocal(index, i) = moleculeDataLocal(index, linkedCellLocal(index) - 1);
+                  linkedCellLocal(index) -= 1;
+                  // decrement iterator as the molecule at position i is now new
+                  i--;
+                }
+              }
+            }); // j, Kokkos::parallel_for
+      } // x
+#if (MD_DIM > 1)
+    } // y
+#endif
+#if (MD_DIM > 2)
+  } // z
+#endif
+}
+
+simplemd::Molecule& simplemd::MoleculeContainer::getMoleculeAt(int i, int j) const { return _moleculeData(i, j); }
+
+simplemd::LinkedCell simplemd::MoleculeContainer::operator[](int idx) {
+  Kokkos::View<Molecule*> lcMoleculeSlice(_moleculeData, idx, Kokkos::ALL);
+  Kokkos::View<int> lcSizeSlice(linkedCellNumMolecules, idx);
+  return simplemd::LinkedCell(lcSizeSlice, lcMoleculeSlice);
+}
+
+int simplemd::MoleculeContainer::getNumCells() const { return linkedCellNumMolecules.size(); }
+
+unsigned int simplemd::MoleculeContainer::positionToCellIndex(const tarch::la::Vector<MD_DIM, double>& position) const {
+  for (unsigned int d = 0; d < MD_DIM; d++) {
+#if (MD_ERROR == MD_YES)
+    if ((position[d] < _domainOffset[d] - _meshWidth[d]) || (position[d] > _domainOffset[d] + _domainSize[d] + _meshWidth[d])) {
+      std::cout << "ERROR simplemd::MoleculeContainer::positionToCellIndex: Position ";
+      std::cout << d << " is out of range!" << std::endl;
+      std::cout << "Position: " << position << std::endl;
+      exit(EXIT_FAILURE);
+    }
+#endif
+  }
+  tarch::la::Vector<MD_DIM, unsigned int> cellVectorIndex(0);
+
+  // determine current cell index (in serial, i.e. 1-D, form)
+  for (unsigned int d = 0; d < MD_DIM; d++) {
+    // find global cell index
+
+    int index = (int)(floor((position[d] - _domainOffset[d]) / _meshWidth[d]));
+    // shift into local cell index
+    index += _localIndexOfFirstCell[d];
+    index -= _globalIndexOfFirstCell[d];
+#if (MD_ERROR == MD_YES)
+    if (index < 0) {
+      std::cout << "ERROR simplemd::MoleculeContainer::positionToCellIndex: index < 0: index=";
+      std::cout << index << std::endl;
+      std::cout << "Dimension : " << d << "," << _globalIndexOfFirstCell[d] << "," << _localIndexOfFirstCell[d] << std::endl;
+      std::cout << (int)(floor((position[d] - _domainOffset[d]) / _meshWidth[d])) << std::endl;
+      for (unsigned int e = 0; e < d; e++) {
+        std::cout << cellVectorIndex[e] << std::endl;
+      }
+      std::cout << "Position: " << position << ", offset: " << _domainOffset << ", meshwidth: " << _meshWidth << std::endl;
+      exit(EXIT_FAILURE);
+    }
+#endif
+    cellVectorIndex[d] = (unsigned int)index;
+  }
+  return vectorIndexToLinear(cellVectorIndex);
+}
+
+const unsigned int simplemd::MoleculeContainer::vectorIndexToLinear(const tarch::la::Vector<MD_DIM, unsigned int>& vectorIndex) const {
+  unsigned int cellLinearIndex = 0;
+  unsigned int stepSize = 1;
+  for (int d = 0; d < MD_DIM; d++) {
+    cellLinearIndex += vectorIndex[d] * stepSize;
+    stepSize *= _numCells[d];
+  }
+  return cellLinearIndex;
+}
--- simplemd/MoleculeContainer.h
+++ simplemd/MoleculeContainer.h
@@ -1,12 +1,6 @@
 #ifndef _MOLECULARDYNAMICS_MOLECULARCONTAINER_H_
 #define _MOLECULARDYNAMICS_MOLECULARCONTAINER_H_
 
-#include <iostream>
-#include <vector>
-#include <sstream>
-#include <algorithm>
-#include <numeric>
-
 #include <Kokkos_Core.hpp>
 
 #include <simplemd/MolecularDynamicsDefinitions.h>
@@ -18,207 +12,126 @@ namespace simplemd {
 class MoleculeContainer;
 }
 
+/**
+ * @brief Contains molecules, creates linked cells and manages its own memory.
+ *
+ * The MoleculeContainer class serves as the sole container responsible for all molecule-related work.
+ * Molecules are stored in a 2D Kokkos View, where each row corresponds to a linked cell (ghost included).
+ * While creating a container, the maximum possible capacity (in molecules) of any cell is defined by cellCapacity.
+ * Thus, each linked cell has a numMolecules() <= capacity.
+ * All data with an index between numMolecules and capacity is treated as garbage data.
+ */
 class simplemd::MoleculeContainer {
 public:
-  MoleculeContainer(simplemd::services::ParallelTopologyService parallelTopologyService, int cellCapacity)
-      : _numCells(parallelTopologyService.getLocalNumberOfCells(true)), _cellCapacity(cellCapacity), moleculeData("moleculeData", parallelTopologyService.getLocalNumberOfCellsLinear(true), cellCapacity), linkedCellNumMolecules("linkedCellNumMolecules", parallelTopologyService.getLocalNumberOfCellsLinear(true)) {
-    tarch::la::Vector<MD_DIM, unsigned int> bufferGlobal(0);
-    tarch::la::Vector<MD_DIM, unsigned int> bufferLocal(0);
-    _domainOffset = parallelTopologyService.getGlobalDomainOffset();
-    _meshWidth = parallelTopologyService.getMeshWidth();
-    bufferGlobal = parallelTopologyService.getGlobalIndexOfFirstCell();
-    bufferLocal = tarch::la::Vector<MD_DIM, unsigned int>(1);
-    for (unsigned int d = 0; d < MD_DIM; d++) {
-      _globalIndexOfFirstCell[d] = (int)bufferGlobal[d];
-      _localIndexOfFirstCell[d] = (int)bufferLocal[d];
-    }
+  /**
+   * @brief Construct a new MoleculeContainer object
+   *
+   * Uses parallelTopologyService to store relevant information related to spatial layout,
+   * so that 3D coordinated can be converted to 3D and 1D linked cell indices.
+   *
+   * @param parallelTopologyService Used to extract and store local number of cells, domain offset etc.
+   * @param cellCapacity The maximum capacity of any cell. Cannot be changed, must provide ample room at compiletime.
+   */
+  MoleculeContainer(simplemd::services::ParallelTopologyService parallelTopologyService, int cellCapacity);
 
-  }
+  /**
+   * @brief Inserts a molecule into a specific linked cell.
+   *
+   * @param cellIdx The one-dimensional index of the linked cell to insert the molecule into (ghost included).
+   * @param molecule The molecule to be inserted.
+   */
+  void insert(int cellIdx, simplemd::Molecule& molecule);
 
-  void insert(int cellIdx, Molecule& molecule) {
-    moleculeData(cellIdx, linkedCellNumMolecules(cellIdx)) = molecule;
-    linkedCellNumMolecules(cellIdx) += 1;
-  }
+  /**
+   * @brief Inserts a molecule into the container.
+   *
+   * The function calculates the appropriate linked cell to insert the molecule into.
+   *
+   * @param molecule The molecule to be inserted.
+   */
+  void insert(simplemd::Molecule& molecule);
 
-  void remove(int cellIdx, int moleculeIdx) {
-    moleculeData(cellIdx, moleculeIdx) = moleculeData(cellIdx, linkedCellNumMolecules(cellIdx) - 1);
-    linkedCellNumMolecules(cellIdx) -= 1;
-  }
+  /**
+   * @brief Removes a molecule, given its ID and linked cell index.
+   *
+   * This is done by swapping the last molecule of the linked cell into position moleculeIdx,
+   * and decrementing the number of molecules in this cell.
+   *
+   * @param cellIdx 1D linked cell index of the molecule (ghost included).
+   * @param moleculeIdx Index of the molecule within the linked cell.
+   */
+  void remove(int cellIdx, int moleculeIdx);
 
-  void clearLinkedCell(int cellIdx) { linkedCellNumMolecules(cellIdx) = 0; }
+  /**
+   * @brief Clears a linked cell.
+   *
+   * This function simply sets the number of molecules in the linked cell as 0.
+   * All data in the linked cell is treated as garbage data, and insertion starts from index 0
+   * within the linked cell again, overwriting previous data.
+   *
+   * @param cellIdx 1D index of the linked cell to clear (ghost included).
+   */
+  void clearLinkedCell(int cellIdx);
 
-  void sort(int cellIdx) { // set all outgoing molecules
-    for (size_t i = 0; i < linkedCellNumMolecules(cellIdx); i++) {
-      int curMolIdx = positionToCellIndex(moleculeData(cellIdx, i).getPosition());
-      if (curMolIdx != cellIdx) { // if molecule does not belong to current cell anymore
-        // write data to target end
-        moleculeData(curMolIdx, linkedCellNumMolecules(curMolIdx)) = moleculeData(cellIdx, i);
-        // increment target end
-        linkedCellNumMolecules(curMolIdx)++;
-        // delete molecule at own position
-        remove(cellIdx, i);
-        // decrement iterator as the molecule at position i is now new
-        i--;
-      }
-    }
-  }
+  /**
+   * @brief Removes all outgoing molecules from a linked cell and moves them to the appropriate destination.
+   *
+   * @param cellIdx 1D index of the linked cell to sort (ghost included).
+   */
+  void sort(int cellIdx);
 
-  void sort() {
-    // sort all inner cells, exclude all ghost cells
-    // find red-black cells
+  /**
+   * @brief Puts all molecules into their appropriate linked cells.
+   *
+   * Since a molecule can only travel to its immediate neighbours per timestep, sorting is done with a red-black
+   * traversal of the whole domain (including ghost cells). This ensures that there are no concurrency issues since
+   * no two linked cells should try to write to the same linked cell. Concurrency is implemented in quarter shells.
+   */
+  void sort();
 
-// iterate over the domain in a red-black manner
-#if (MD_DIM > 2)
-    for (unsigned int z = 0; z < 2; z++) {
-#endif
-#if (MD_DIM > 1)
-      for (unsigned int y = 0; y < 2; y++) {
-#endif
-        for (unsigned int x = 0; x < 2; x++) {
-          // determine range/ length of blocks for red-black traversal.
-          // For odd block sizes, we need to do some more work in the
-          // x/y/z==0-traversals. The second x/y/z==1-traversals are reduced by
-          // the normal integer-rounding in this case.
-          const tarch::la::Vector<MD_DIM, unsigned int> lengthVector((_numCells[0]-2 + ((_numCells[0]-2) % 2) * (x == 0)) / 2
-#if (MD_DIM > 1)
-                                                                     ,
-                                                                     (_numCells[1]-2 + ((_numCells[1]-2) % 2) * (y == 0)) / 2
-#endif
-#if (MD_DIM > 2)
-                                                                     ,
-                                                                     (_numCells[2]-2 + ((_numCells[2]-2) % 2) * (z == 0)) / 2
-#endif
-          );
-          const int length = lengthVector[0]
-#if (MD_DIM > 1)
-                             * lengthVector[1]
-#endif
-#if (MD_DIM > 2)
-                             * lengthVector[2]
-#endif
-              ;
+  /**
+   * @brief Get the Molecule at linked cell index i (ghost included), position j
+   *
+   * @param i
+   * @param j
+   * @return simplemd::Molecule&
+   */
+  simplemd::Molecule& getMoleculeAt(int i, int j) const;
 
-          // parallelise loop for all cells that are to be traversed in this way
-          Kokkos::parallel_for(
-              length, KOKKOS_LAMBDA(const unsigned int j) {
-                // compute index of the current cell
-                unsigned int index = 0;
-#if (MD_DIM > 1)
-                int helpIndex1 = j;
-                int helpIndex2 = 0;
-#endif
+  /**
+   * @brief Returns the linked cell at index idx (ghost included)
+   *
+   * @param idx
+   * @return simplemd::LinkedCell
+   */
+  simplemd::LinkedCell operator[](int idx);
 
-#if (MD_DIM > 2)
-                // determine plane within traversed block
-                helpIndex2 = helpIndex1 / (lengthVector[0] * lengthVector[1]);
-                // save rest of index in helpIndex1
-                helpIndex1 = helpIndex1 - helpIndex2 * (lengthVector[0] * lengthVector[1]);
-                // compute contribution to index (the starting 1 is the z coordinate of the first cell)
-                index += (_localIndexOfFirstCell[2] + 2 * helpIndex2 + z) * _numCells[0] * _numCells[1];
-#endif
-#if (MD_DIM > 1)
-                // determine plane within traversed block
-                helpIndex2 = helpIndex1 / lengthVector[0];
-                // save rest of index in helpIndex1
-                helpIndex1 = helpIndex1 - helpIndex2 * lengthVector[0];
-                // compute contribution to index
-                index += (_localIndexOfFirstCell[1] + 2 * helpIndex2 + y) * _numCells[0];
-                // compute contribution for last dimension
-                index += (_localIndexOfFirstCell[0] + 2 * helpIndex1 + x);
-#else
-        index = _localIndexOfFirstCell[0] + 2 * j + x;
-#endif
-#if (MD_DEBUG == MD_YES)
-                std::cout << "Handle cell " << index << std::endl;
-#endif
-                auto linkedCellLocal(linkedCellNumMolecules);
-                auto moleculeDataLocal(moleculeData);
-                for (size_t i = 0; i < linkedCellLocal(index); i++) {
-                  int curMolIdx = positionToCellIndex(moleculeDataLocal(index, i).getPosition());
-                  if (curMolIdx != index) { // if molecule does not belong to current cell anymore
-                    // write data to target end
-                    moleculeDataLocal(curMolIdx, linkedCellLocal(curMolIdx)) = moleculeDataLocal(index, i);
-                    // increment target end
-                    linkedCellLocal(curMolIdx)++;
-                    // delete molecule at own position
-                    moleculeDataLocal(index, i) = moleculeDataLocal(index, linkedCellLocal(index) - 1);
-                    linkedCellLocal(index) -= 1;
-                    // decrement iterator as the molecule at position i is now new
-                    i--;
-                  }
-                }
-              }); // j, Kokkos::parallel_for
-        } // x
-#if (MD_DIM > 1)
-      } // y
-#endif
-#if (MD_DIM > 2)
-    } // z
-#endif
-  }
-
-  simplemd::Molecule& getMoleculeAt(int i, int j) const { return moleculeData(i, j); }
-
-  simplemd::LinkedCell operator[](int idx) {
-    Kokkos::View<Molecule*> lcMoleculeSlice(moleculeData, idx, Kokkos::ALL);
-    Kokkos::View<int> lcSizeSlice(linkedCellNumMolecules, idx);
-    return simplemd::LinkedCell(lcSizeSlice, lcMoleculeSlice);
-  }
-
-  int getNumCells() const { return linkedCellNumMolecules.size(); }
-
-  Kokkos::View<simplemd::Molecule**> moleculeData;
-  Kokkos::View<int*> linkedCellNumMolecules;
+  /**
+   * @brief Get the total number of cells in the container
+   *
+   * @return int
+   */
+  int getNumCells() const;
 
 private:
-  unsigned int positionToCellIndex(const tarch::la::Vector<MD_DIM, double>& position) const {
-    for (unsigned int d = 0; d < MD_DIM; d++) {
-  #if (MD_ERROR == MD_YES)
-      if ((position[d] < _domainOffset[d] - _meshWidth[d]) || (position[d] > _domainOffset[d] + _domainSize[d] + _meshWidth[d])) {
-        std::cout << "ERROR simplemd::MoleculeContainer::positionToCellIndex: Position ";
-        std::cout << d << " is out of range!" << std::endl;
-        std::cout << "Position: " << position << std::endl;
-        exit(EXIT_FAILURE);
-      }
-  #endif
-    }
-    tarch::la::Vector<MD_DIM, unsigned int> cellVectorIndex(0);
+  /**
+   * @brief Converts a global 3D spatial coordinate to a local 1D linked cell index.
+   *
+   * This can be used to find the linked cell that a molecule belongs to.
+   *
+   * @param position 3D spatial coordinate.
+   * @return unsigned int
+   */
+  unsigned int positionToCellIndex(const tarch::la::Vector<MD_DIM, double>& position) const;
 
-    // determine current cell index (in serial, i.e. 1-D, form)
-    for (unsigned int d = 0; d < MD_DIM; d++) {
-      // find global cell index
+  /**
+   * @brief Converts a 3D local linked cell index into a 1D local linked cell index.
+   *
+   * @param vectorIndex 3D local index of the linked cell.
+   * @return const unsigned int
+   */
+  const unsigned int vectorIndexToLinear(const tarch::la::Vector<MD_DIM, unsigned int>& vectorIndex) const;
 
-      int index = (int)(floor((position[d] - _domainOffset[d]) / _meshWidth[d]));
-      // shift into local cell index
-      index += _localIndexOfFirstCell[d];
-      index -= _globalIndexOfFirstCell[d];
-  #if (MD_ERROR == MD_YES)
-      if (index < 0) {
-        std::cout << "ERROR simplemd::MoleculeContainer::positionToCellIndex: index < 0: index=";
-        std::cout << index << std::endl;
-        std::cout << "Dimension : " << d << "," << _globalIndexOfFirstCell[d] << "," << _localIndexOfFirstCell[d] << std::endl;
-        std::cout << (int)(floor((position[d] - _domainOffset[d]) / _meshWidth[d])) << std::endl;
-        for (unsigned int e = 0; e < d; e++) {
-          std::cout << cellVectorIndex[e] << std::endl;
-        }
-        std::cout << "Position: " << position << ", offset: " << _domainOffset << ", meshwidth: " << _meshWidth << std::endl;
-        exit(EXIT_FAILURE);
-      }
-  #endif
-      cellVectorIndex[d] = (unsigned int)index;
-    }
-    return vectorIndexToLinear(cellVectorIndex);
-  }
-
-  const unsigned int vectorIndexToLinear(const tarch::la::Vector<MD_DIM, unsigned int>& vectorIndex) const {
-    unsigned int cellLinearIndex = 0;
-    unsigned int stepSize = 1;
-    for (int d = 0; d < MD_DIM; d++) {
-      cellLinearIndex += vectorIndex[d] * stepSize;
-      stepSize *= _numCells[d];
-    }
-    return cellLinearIndex;
-  }
   /** number of cells per direction in the local domain */
   tarch::la::Vector<MD_DIM, unsigned int> _numCells;
 
@@ -238,5 +151,8 @@ private:
 
   /** local index of the first cell within this domain */
   tarch::la::Vector<MD_DIM, int> _localIndexOfFirstCell;
+
+  Kokkos::View<simplemd::Molecule**> _moleculeData;
+  Kokkos::View<int*> linkedCellNumMolecules;
 };
 #endif // _MOLECULARDYNAMICS_MOLECULARCONTAINER_H_
