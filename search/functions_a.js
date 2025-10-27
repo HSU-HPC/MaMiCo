ae089beefad416bfafc4a24368bd5fce83d6b15a
Thinkpiet <Thinkpiet@users.noreply.github.com>
2025-10-27 17:40:56 +0000

    deploy: cee6295ca7de85f551b5775c82e891a51cfd789c

--- search/functions_a.js
+++ search/functions_a.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['lastchild_0',['LastChild',['../classtinyxml2_1_1XMLNode.html#a69a29bb8263ff5e9815be180bf27e7af',1,'tinyxml2::XMLNode::LastChild()'],['../classtinyxml2_1_1XMLHandle.html#a9d09f04435f0f2f7d0816b0198d0517b',1,'tinyxml2::XMLHandle::LastChild()']]],
   ['lastchildelement_1',['LastChildElement',['../classtinyxml2_1_1XMLNode.html#a609e02f02044f39b928d1a3e0de9f532',1,'tinyxml2::XMLNode::LastChildElement()'],['../classtinyxml2_1_1XMLHandle.html#a42cccd0ce8b1ce704f431025e9f19e0c',1,'tinyxml2::XMLHandle::LastChildElement()']]],
-  ['lbcouettesolver_2',['LBCouetteSolver',['../classcoupling_1_1solvers_1_1LBCouetteSolver.html#af10faf0866b869f325e6acee3b131e5d',1,'coupling::solvers::LBCouetteSolver']]],
+  ['lbcouettesolver_2',['LBCouetteSolver',['../classcoupling_1_1solvers_1_1LBCouetteSolver.html#ae9628998145318f08bd65520c478c406',1,'coupling::solvers::LBCouetteSolver']]],
   ['lbcouettesolverinterface_3',['LBCouetteSolverInterface',['../classcoupling_1_1solvers_1_1LBCouetteSolverInterface.html#a48b23b24457671e88e91ec06ae17c7fe',1,'coupling::solvers::LBCouetteSolverInterface']]],
   ['linkedcellcontainer_4',['LinkedCellContainer',['../classcoupling_1_1datastructures_1_1LinkedCellContainer.html#a419c3908aab50875f9dbfd40c7d5b612',1,'coupling::datastructures::LinkedCellContainer']]],
   ['load_5',['load',['../classtarch_1_1configuration_1_1ParseConfiguration_1_1XMLConfiguration.html#a03e0283dda4335736e6d00557866a76c',1,'tarch::configuration::ParseConfiguration::XMLConfiguration::load()'],['../classParseConfiguration_1_1XMLConfiguration.html#a03e0283dda4335736e6d00557866a76c',1,'ParseConfiguration::XMLConfiguration::load()']]],
--- search/variables_b.js
+++ search/variables_b.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['ploteverytimestep_0',['plotEveryTimestep',['../structcoupling_1_1configurations_1_1CouetteConfig.html#af03077aebac8f701e728a1bd1f1a374f',1,'coupling::configurations::CouetteConfig']]]
+  ['plotaveragevelocity_0',['plotAverageVelocity',['../structcoupling_1_1configurations_1_1CouetteConfig.html#ab208d38ee532b34c1e4907888d58d92d',1,'coupling::configurations::CouetteConfig']]],
+  ['ploteverytimestep_1',['plotEveryTimestep',['../structcoupling_1_1configurations_1_1CouetteConfig.html#af03077aebac8f701e728a1bd1f1a374f',1,'coupling::configurations::CouetteConfig']]]
 ];
