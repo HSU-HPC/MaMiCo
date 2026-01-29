3854d47f81247a2a532e46652832b1316fde64a4
Piet Jarmatz <jarmatz@hsu-hh.de>
2026-01-29 12:50:16 +0100

    main_couette outputs concurrency now

--- coupling/scenario/main_couette.cpp
+++ coupling/scenario/main_couette.cpp
@@ -32,6 +32,7 @@ int main(int argc, char* argv[]) {
   std::cout << "Kokkos using execution space \"" << MainExecSpace::name() << "\" with memory space \"" << MainExecSpace::memory_space::name() << "\""
             << std::endl;
   MainExecSpace().print_configuration(std::cout);
+  std::cout << "Available concurrency: " << MainExecSpace::concurrency() << std::endl;
 
   // run scenarios
   runScenario(new CouetteScenario());
