f0795c616747f2484ddcf0b04ca11df82fd46b87
Ruben Horn <>
2026-02-12 16:15:46 +0100

    De-duplicated fixed-point limits definition

--- tarch/utils/Utils.h
+++ tarch/utils/Utils.h
@@ -4,6 +4,12 @@
  * @author Piet Jarmatz
  */
 
+#define DEFINE_DECIMAL_FP6_LIMITS() \
+    constexpr double maxFP6 = 1e6; \
+    constexpr double stepFP6 = (double)(std::numeric_limits<long long>::max()) / maxFP6; \
+    constexpr double minFP6 = 1 / stepFP6; \
+    (void)minFP6; // Avoid unused variable warning/error if minFP6 is not needed.
+
 namespace tarch {
 namespace utils {
 
