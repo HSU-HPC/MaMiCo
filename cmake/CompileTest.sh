7a738aa8e71bd49c18bf2fc1613d5c9a9c6cf996
Ruben Horn <>
2026-08-14 09:51:28 +0200

    CompileTest.sh timing

--- cmake/CompileTest.sh
+++ cmake/CompileTest.sh
@@ -12,17 +12,22 @@ for build in $2; do
 		}
 
 		for mpi in ON OFF; do
+			echo "CompileTest: Running test with build type=${build}, compiler=${compiler}, MPI=${mpi}"
+			start=$SECONDS
+			
 			cmake . -D CMAKE_CXX_COMPILER=${compiler} -D BUILD_WITH_MPI=${mpi} -D BUILD_WITH_PYBIND11=${mpi} -D CMAKE_BUILD_TYPE=${build} || { #try/catch
 			>&2 echo "CompileTest: Makefile generation of CouetteTest failed for compiler: ${compiler} with MPI ${mpi}"
 			exit 1 
 			}
 
-			make clean
-
 			make -j4 || { #try/catch
 				>&2 echo "CompileTest: Compilation of CouetteTest failed for compiler: ${compiler} with MPI ${mpi}" 
 				exit 1 
 			}
+
+			elapsed=$((SECONDS - start))
+			make clean # Clean up after timing
+    		echo "CompileTest: Test with build type=${build}, compiler=${compiler}, MPI=${mpi} took ${elapsed}s"
 		done
 	done
 done
