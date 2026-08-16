396af1a8c077cee04cab667b928152bb7f1144bf
amartyads <amartyads@gmail.com>
2026-08-16 13:30:20 +0200

    Consistency with master

---
+++ coupling/scenario/main_cfdpush.cpp
@@ -0,0 +1,49 @@
+// This file is part of the Mamico project. For conditions of distribution
+// and use, please see the copyright notice in Mamico's main folder, or at
+// www5.in.tum.de/mamico
+#ifndef _MAIN_CFDPUSH_CPP_
+#define _MAIN_CFDPUSH_CPP_
+
+#include "coupling/CouplingMDDefinitions.h"
+#include "coupling/scenario/CFDPushScenario.h"
+#include <cstdlib>
+#include <iostream>
+#if (COUPLING_MD_PARALLEL == COUPLING_MD_YES)
+#include <mpi.h>
+#endif
+
+/** executes a newly created scenario and deletes it immediately again. */
+void runScenario(Scenario* scenario) {
+  if (scenario == NULL) {
+    std::cout << "ERROR executeScenario: scenario==NULL!" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  scenario->run();
+  delete scenario;
+}
+
+int main(int argc, char* argv[]) {
+
+#if (COUPLING_MD_PARALLEL == COUPLING_MD_YES)
+  MPI_Init(&argc, &argv);
+#endif
+
+  Kokkos::ScopeGuard kokkos(argc, argv);
+  MainExecSpace mainExecSpace;
+  std::cout << "Kokkos using execution space \"" << mainExecSpace.name() << "\" with memory space \"" << MainExecSpace::memory_space::name() << "\""
+            << std::endl;
+  mainExecSpace.print_configuration(std::cout);
+
+  std::cout << "Available concurrency: " << mainExecSpace.concurrency() << std::endl;
+
+  // run scenarios
+  runScenario(new CFDPushScenario());
+
+#if (COUPLING_MD_PARALLEL == COUPLING_MD_YES)
+  MPI_Finalize();
+#endif
+
+  return 0;
+}
+
+#endif // _MAIN_CFDPUSH_CPP_
