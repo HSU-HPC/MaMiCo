58721c9c391d07d090a72af7246c9d19a9325b35
amartyads <amartyads@gmail.com>
2026-09-18 23:51:48 +0200

    New test, passing

--- test/unit/coupling/interface/impl/ls1/LS1MoleculeTest.cpp
+++ test/unit/coupling/interface/impl/ls1/LS1MoleculeTest.cpp
@@ -6,6 +6,8 @@
 #include "coupling/CouplingMDDefinitions.h"
 #include "coupling/interface/impl/ls1/LS1Molecule.h"
 
+#include "ls1/src/Simulation.h"
+
 #include <sstream>
 
 #define MY_LINKEDCELL ls1::LS1RegionWrapper
@@ -15,11 +17,17 @@ class LS1MoleculeTest : public CppUnit::TestFixture {
   CPPUNIT_TEST(testPosition);
   CPPUNIT_TEST(testVelocity);
   CPPUNIT_TEST(testForce);
+  CPPUNIT_TEST(testGetPotentialEnergy);
   CPPUNIT_TEST(testSetPotentialEnergy);
   CPPUNIT_TEST_SUITE_END();
 
 public:
   void setUp() {
+#ifdef MARDYN_AUTOPAS
+    _ls1ConfigFileName = "../test/unit/coupling/interface/impl/ls1/autopasgridconfig.xml";
+#else
+    _ls1ConfigFileName = "../test/unit/coupling/interface/impl/ls1/ls1gridconfig.xml";
+#endif
     Log::global_log = std::make_unique<Log::Logger>(Log::None);
     Log::global_log->set_mpi_output_root(0);
     coupling::interface::LS1StaticCommData::getInstance().setBoxOffsetAtDim(0, 0);
@@ -28,6 +36,15 @@ public:
 #if (COUPLING_MD_PARALLEL == COUPLING_MD_YES)
     coupling::interface::LS1StaticCommData::getInstance().setLocalCommunicator(MPI_COMM_WORLD);
 #endif
+    _testSimulation = new Simulation();
+    global_simulation = _testSimulation;
+    _testSimulation->disableFinalCheckpoint();
+    _testSimulation->readConfigFile(_ls1ConfigFileName);
+    //_testSimulation->getDomain()->thermostatOff();
+    //_testSimulation->getDomain()->setExplosionHeuristics(false);
+    // after this point the mamico plugin exists and is accessible
+    _testSimulation->prepare_start();
+    _testSimulation->preSimLoopSteps();
   }
   void tearDown() {}
   void testPosition() {
@@ -97,11 +114,32 @@ public:
     }
   }
 
+  void testGetPotentialEnergy() {
+    ::Molecule ls1Molecule;
+    coupling::interface::LS1Molecule mamicoMolecule(&ls1Molecule);
+    ls1::LS1RegionWrapper region;
+    for(int i = 0; i < 40; i++) {
+      for(int j = 0; j < 40; j++) {
+        for(int k = 0; k < 40; k++) {
+          tarch::la::Vector<3, double> position{i + 0.5, j + 0.5, k + 0.5};
+          mamicoMolecule.setPosition(position);
+          const double potFromMolecule = mamicoMolecule.getPotentialEnergy();
+          const auto [force, potFromIterator] = region.calculateForceAndPotentialAtPoint(position, true, true, false);
+          CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("Potential assersion", potFromIterator, potFromMolecule, 1e-6);
+        }
+      }
+    }
+  }
+
   void testSetPotentialEnergy() {
     ::Molecule ls1Molecule;
     coupling::interface::LS1Molecule mamicoMolecule(&ls1Molecule);
     CPPUNIT_ASSERT_THROW(mamicoMolecule.setPotentialEnergy(0.0), std::runtime_error);
   }
+
+private:
+  Simulation* _testSimulation;
+  std::string _ls1ConfigFileName;
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(LS1MoleculeTest);
